0d56b5a84aba8364c9f5694c93d3508c70ce69ea
SilviaAmAm <silvia@maykinmedia.nl>
2024-05-08 12:43:01 +0200

    :sparkles: [maykinmedia/archiefbeheercomponent#345] Add list endpoint for destruction lists

--- backend/src/openarchiefbeheer/destruction/api/viewsets.py
+++ backend/src/openarchiefbeheer/destruction/api/viewsets.py
@@ -1,5 +1,7 @@
 from django.db import transaction
+from django.utils.translation import gettext_lazy as _
 
+from drf_spectacular.utils import OpenApiExample, extend_schema, extend_schema_view
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,7 +10,42 @@ from .permissions import CanStartDestructionPermission
 from .serializers import DestructionListSerializer
 
 
-class DestructionListViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+@extend_schema_view(
+    list=extend_schema(
+        summary=_("List destruction lists"),
+        description=_("List all destruction lists."),
+    ),
+    create=extend_schema(
+        summary=_("Create destruction list"),
+        description=_("Create a new destruction list."),
+        examples=[
+            OpenApiExample(
+                name="Example list creation",
+                value={
+                    "name": "An example list",
+                    "containsSensitiveInfo": True,
+                    "assignees": [
+                        {"user": 1, "order": 0},
+                        {"user": 2, "order": 1},
+                    ],
+                    "items": [
+                        {
+                            "zaak": "http://some-zaken-api.nl/zaken/api/v1/zaken/111-111-111",
+                            "extraZaakData": {},
+                        },
+                        {
+                            "zaak": "http://some-zaken-api.nl/zaken/api/v1/zaken/222-222-222",
+                            "extraZaakData": {},
+                        },
+                    ],
+                },
+            )
+        ],
+    ),
+)
+class DestructionListViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
     serializer_class = DestructionListSerializer
     queryset = DestructionList.objects.all()
 
