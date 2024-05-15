7abe5c91bf0f7031a764a036ac95c5d9c65e2052
SilviaAmAm <silvia@maykinmedia.nl>
2024-05-15 11:25:47 +0200

    :sparkles: Add trait to factory

--- backend/src/openarchiefbeheer/zaken/tests/factories.py
+++ backend/src/openarchiefbeheer/zaken/tests/factories.py
@@ -20,3 +20,28 @@ class ZaakFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Zaak
+
+    class Params:
+        with_expand = factory.Trait(
+            _expand={
+                "zaaktype": {
+                    "url": "http://catalogue-api.nl/zaaktypen/111-111-111",
+                    "selectielijst_procestype": {"nummer": 1},
+                },
+                "resultaat": {
+                    "resultaattype": "http://catalogue-api.nl/catalogi/api/v1/resultaattypen/111-111-111",
+                    "_expand": {
+                        "resultaattype": {
+                            "url": "http://catalogue-api.nl/catalogi/api/v1/resultaattypen/111-111-111",
+                            "archiefactietermijn": "P1D",
+                        }
+                    },
+                },
+            }
+        )
+        with_related_zaken = factory.Trait(
+            relevante_andere_zaken=[
+                "http://zaken-api.nl/zaken/api/v1/zaken/111-111-111",
+                "http://zaken-api.nl/zaken/api/v1/zaken/222-222-222",
+            ]
+        )
