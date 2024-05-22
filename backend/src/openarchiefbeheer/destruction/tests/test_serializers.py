48c087752c1987627701f9b3d69c2adbb05dcafb
SilviaAmAm <silvia@maykinmedia.nl>
2024-05-22 11:49:06 +0200

    :white_check_mark: Test update list endpoint

--- backend/src/openarchiefbeheer/destruction/tests/test_serializers.py
+++ backend/src/openarchiefbeheer/destruction/tests/test_serializers.py
@@ -13,7 +13,10 @@ from timeline_logger.models import TimelineLog
 from openarchiefbeheer.accounts.tests.factories import UserFactory
 from openarchiefbeheer.destruction.api.serializers import DestructionListSerializer
 from openarchiefbeheer.destruction.constants import ListItemStatus
-from openarchiefbeheer.destruction.tests.factories import DestructionListItemFactory
+from openarchiefbeheer.destruction.tests.factories import (
+    DestructionListFactory,
+    DestructionListItemFactory,
+)
 from openarchiefbeheer.emails.models import EmailConfig
 
 factory = APIRequestFactory()
@@ -201,3 +204,112 @@ class DestructionListSerializerTests(TestCase):
         serializer = DestructionListSerializer(data=data, context={"request": request})
 
         self.assertTrue(serializer.is_valid())
+
+    def test_full_list_update(self):
+        user1 = UserFactory.create(
+            username="reviewer1", role__can_review_destruction=True
+        )
+        user2 = UserFactory.create(
+            username="reviewer2", role__can_review_destruction=True
+        )
+        user3 = UserFactory.create(
+            username="reviewer3", role__can_review_destruction=True
+        )
+
+        destruction_list = DestructionListFactory.create(
+            name="A test list", contains_sensitive_info=True
+        )
+        destruction_list.bulk_create_assignees(
+            [{"user": user1, "order": 0}, {"user": user2, "order": 1}]
+        )
+        DestructionListItemFactory.create_batch(
+            2,
+            destruction_list=destruction_list,
+            status=ListItemStatus.suggested,
+        )
+
+        data = {
+            "name": "An updated test list",
+            "contains_sensitive_info": False,
+            "assignees": [
+                {"user": user1.pk, "order": 0},
+                {"user": user3.pk, "order": 1},
+            ],
+            "items": [
+                {
+                    "zaak": "http://localhost:8003/zaken/api/v1/zaken/111-111-111",
+                    "extra_zaak_data": {"key": "value"},
+                },
+            ],
+        }
+
+        serializer = DestructionListSerializer(instance=destruction_list, data=data)
+        self.assertTrue(serializer.is_valid())
+
+        with freeze_time("2024-05-02T16:00:00+02:00"):
+            serializer.save()
+
+        destruction_list.refresh_from_db()
+
+        self.assertEqual(destruction_list.name, "An updated test list")
+
+        items = destruction_list.items.all()
+
+        self.assertEqual(items.count(), 1)
+        self.assertEqual(items[0].extra_zaak_data["key"], "value")
+
+        assignees = destruction_list.assignees.all().order_by("order")
+
+        self.assertEqual(assignees[0].user.pk, user1.pk)
+        self.assertEqual(assignees[1].user.pk, user3.pk)
+
+        logs = TimelineLog.objects.filter(
+            template="logging/destruction_list_updated.txt"
+        )
+
+        self.assertEqual(logs.count(), 1)
+
+        message = logs[0].get_message()
+
+        self.assertEqual(
+            message,
+            '[2024-05-02T16:00:00+02:00]: Destruction list "An updated test list" was updated.',
+        )
+
+    def test_partial_list_update(self):
+        user1 = UserFactory.create(
+            username="reviewer1", role__can_review_destruction=True
+        )
+        user2 = UserFactory.create(
+            username="reviewer2", role__can_review_destruction=True
+        )
+
+        destruction_list = DestructionListFactory.create(
+            name="A test list", contains_sensitive_info=True
+        )
+        destruction_list.bulk_create_assignees(
+            [{"user": user1, "order": 0}, {"user": user2, "order": 1}]
+        )
+        DestructionListItemFactory.create_batch(
+            2,
+            destruction_list=destruction_list,
+            status=ListItemStatus.suggested,
+        )
+
+        data = {
+            "name": "An updated test list",
+        }
+
+        serializer = DestructionListSerializer(
+            instance=destruction_list, data=data, partial=True
+        )
+
+        self.assertTrue(serializer.is_valid())
+
+        serializer.save()
+
+        destruction_list.refresh_from_db()
+
+        self.assertEqual(destruction_list.name, "An updated test list")
+        self.assertEqual(destruction_list.items.all().count(), 2)
+        self.assertEqual(destruction_list.assignees.all().count(), 2)
