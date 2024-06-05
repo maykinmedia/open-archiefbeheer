ca8fbbcdd2b058c8f4c0ee1ca0a1712aac594823
SilviaAmAm <silvia@maykinmedia.nl>
2024-06-05 16:34:04 +0200

    :whale: Get docker image to work

--- backend/bin/celery_beat.sh
+++ backend/bin/celery_beat.sh
@@ -7,8 +7,6 @@ LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 mkdir -p celerybeat
 
 echo "Starting celery beat"
-exec celery beat \
-    --app openarchiefbeheer \
+exec celery --workdir src --app openarchiefbeheer.celery worker \
     -l $LOGLEVEL \
-    --workdir src \
     -s ../celerybeat/beat
