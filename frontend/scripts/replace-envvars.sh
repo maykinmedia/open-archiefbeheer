d39221b13380050ba1a3fa12c5b411e62fdd9711
SilviaAmAm <silvia@maykinmedia.nl>
2024-06-05 16:05:50 +0200

    prev

--- frontend/scripts/replace-envvars.sh
+++ frontend/scripts/replace-envvars.sh
@@ -4,8 +4,6 @@ set -eux -o pipefail
 
 for file in /app/static/js/*.js;
 do
-  sed -i 's|REACT_APP_API_SCHEME_PLACEHOLDER|'${REACT_APP_API_SCHEME}'|g' $file
-  sed -i 's|REACT_APP_API_HOST_PLACEHOLDER|'${REACT_APP_API_HOST}'|g' $file
-  sed -i 's|REACT_APP_API_PORT_PLACEHOLDER|'${REACT_APP_API_PORT}'|g' $file
+  sed -i 's|REACT_APP_API_URL_PLACEHOLDER|'${REACT_APP_API_URL}'|g' $file
   sed -i 's|REACT_APP_API_PATH_PLACEHOLDER|'${REACT_APP_API_PATH}'|g' $file
 done
--- frontend/src/lib/api/request.ts
+++ frontend/src/lib/api/request.ts
@@ -1,19 +1,13 @@
 import { getCookie } from "../cookie/cookie";
 
 /** Scheme for all API requests. */
-export const API_SCHEME = process.env.REACT_APP_API_SCHEME;
-
-/** The host for the API server. */
-export const API_HOST = process.env.REACT_APP_API_HOST;
-
-/** The port for the API server. */
-export const API_PORT = process.env.REACT_APP_API_PORT;
+export const API_URL = process.env.REACT_APP_API_URL;
 
 /** The base path for all API requests. */
 export const API_PATH = process.env.REACT_APP_API_PATH;
 
 /** The base url for all API requests. */
-export const API_BASE_URL = `${API_SCHEME}://${API_HOST}:${API_PORT}${API_PATH}`;
+export const API_BASE_URL = `${API_URL}${API_PATH}`;
 
 /**
  * Makes an actual fetch request to the API, should be used by all other API implementations.
