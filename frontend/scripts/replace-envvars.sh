dddbacce5e18d19d12716e9fea2998e498fbced2
SilviaAmAm <silvia@maykinmedia.nl>
2024-06-05 16:32:50 +0200

    :sparkles: Get frontend to retrieve url from env vars

---
+++ frontend/scripts/replace-envvars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+for file in /app/static/js/*.js;
+do
+  sed -i 's|REACT_APP_API_URL_PLACEHOLDER|'${REACT_APP_API_URL}'|g' $file
+  sed -i 's|REACT_APP_API_PATH_PLACEHOLDER|'${REACT_APP_API_PATH}'|g' $file
+done
