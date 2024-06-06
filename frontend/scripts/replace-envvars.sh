9a5d5d7240795f25629744f44786d2521ea8e418
SilviaAmAm <silvia@maykinmedia.nl>
2024-06-06 10:59:51 +0200

    :sparkles: [#69] Make the link to the external system configurable throufh env vars

--- frontend/scripts/replace-envvars.sh
+++ frontend/scripts/replace-envvars.sh
@@ -6,4 +6,5 @@ for file in /app/static/js/*.js;
 do
   sed -i 's|REACT_APP_API_URL_PLACEHOLDER|'${REACT_APP_API_URL}'|g' $file
   sed -i 's|REACT_APP_API_PATH_PLACEHOLDER|'${REACT_APP_API_PATH}'|g' $file
+  sed -i 's|REACT_APP_ZAAK_URL_TEMPLATE_PLACEHOLDER|'${REACT_APP_ZAAK_URL_TEMPLATE}'|g' $file
 done
