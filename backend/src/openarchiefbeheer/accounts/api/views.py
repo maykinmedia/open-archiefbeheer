ddbdd08edce441585fd9e2060049b3d634506526
Sven van de Scheur <svenvandescheur@gmail.com>
2024-06-18 12:20:48 +0200

    :sparkles: #123 - chore: add WhoAmI endpoint

--- backend/src/openarchiefbeheer/accounts/api/views.py
+++ backend/src/openarchiefbeheer/accounts/api/views.py
@@ -2,7 +2,7 @@ from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from openarchiefbeheer.accounts.models import User
 
@@ -23,3 +23,19 @@ class ReviewersView(ListAPIView):
 
     def get_queryset(self) -> QuerySet[User]:
         return User.objects.reviewers()
+
+
+@extend_schema(
+    summary=_("Who Am I"),
+    description=_(
+        "Returns the current logged in user."
+    ),
+    responses={
+        200: UserSerializer(),
+    },
+)
+class WhoAmIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self) -> User:
+        return self.request.user
