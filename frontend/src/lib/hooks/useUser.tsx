fd68bbcaf13cb4f65d975066208c4572a1b00657
Julian Roeland <julian@maykinmedia.nl>
2024-06-18 13:18:54 +0200

    :recycle: - ref: useAsync

--- frontend/src/lib/hooks/useUser.tsx
+++ frontend/src/lib/hooks/useUser.tsx
@@ -1,25 +1,22 @@
-import { useEffect } from "react";
-import { useSessionStorage } from "react-use";
+import { useAsync, useSessionStorage } from "react-use";
 
 import { User, whoAmI } from "../api/auth";
 
 export const USER_SESSION_KEY = "oab.lib.hooks.user";
 
 export const useUser = (): { user: User | null } => {
+  // Note: Possible improvement -> loading state? But this would require to handle this in the UI aswell. Maybe for the future
   const [user, setUser] = useSessionStorage<User | null>(
     USER_SESSION_KEY,
     null,
   );
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const response = await whoAmI();
-      setUser(response || null);
-    };
-
-    if (!user) {
-      fetchUser();
+  useAsync(async () => {
+    if (user) {
+      return;
     }
+    const response = await whoAmI();
+    setUser(response || null);
   }, [user]);
 
   return { user };
