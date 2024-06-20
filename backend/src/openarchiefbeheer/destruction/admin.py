99730e93e2245eb55a6123bee97e670e162487c7
SilviaAmAm <silvia@maykinmedia.nl>
2024-06-20 11:02:14 +0200

    :sparkles: [#127] Admin interface for the reviews

--- backend/src/openarchiefbeheer/destruction/admin.py
+++ backend/src/openarchiefbeheer/destruction/admin.py
@@ -6,6 +6,8 @@ from .models import (
     DestructionListItem,
     DestructionListItemReview,
     DestructionListReview,
+    ReviewItemResponse,
+    ReviewResponse,
 )
 
 
@@ -59,3 +61,24 @@ class DestructionListItemReviewAdmin(admin.ModelAdmin):
     list_display = ("destruction_list", "destruction_list_item")
     search_fields = ("destruction_list__name", "destruction_list_item__zaak")
     raw_id_fields = ("destruction_list", "review", "destruction_list_item")
+
+
+@admin.register(ReviewResponse)
+class ReviewResponseAdmin(admin.ModelAdmin):
+    list_display = ("review",)
+    search_fields = (
+        "review__author__username",
+        "review__destruction_list__name",
+        "review__destruction_list__author__username",
+    )
+
+
+@admin.register(ReviewItemResponse)
+class ReviewItemResponse(admin.ModelAdmin):
+    list_display = ("review_item", "action_item")
+    search_fields = (
+        "review_item__destruction_list_item__zaak",
+        "review_item__review__author__username",
+        "review_item__destruction_list__author__username",
+        "review_item__destruction_list__name",
+    )
