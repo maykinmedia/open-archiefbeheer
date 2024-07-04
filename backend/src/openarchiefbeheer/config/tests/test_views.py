1912923eabf83f8b17c6508c8056075c9e8fa2b8
SilviaAmAm <silvia@maykinmedia.nl>
2024-07-04 15:45:32 +0200

    :white_check_mark: [#81] Test endpoint to configure archive

---
+++ backend/src/openarchiefbeheer/config/tests/test_views.py
@@ -0,0 +1,64 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from openarchiefbeheer.accounts.tests.factories import UserFactory
+
+from ..models import ArchiveConfig
+
+
+class ArchiveConfigViews(APITestCase):
+    def test_not_authenticated(self):
+        response = self.client.get(reverse("api:archive-config"))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authenticated_can_retrieve(self):
+        user = UserFactory.create()
+
+        self.client.force_login(user)
+        response = self.client.get(reverse("api:archive-config"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("zaaktypesShortProcess", response.json())
+
+    def test_not_record_manager_update(self):
+        user = UserFactory.create(role__can_start_destruction=False)
+
+        self.client.force_login(user)
+        response = self.client.put(
+            reverse("api:archive-config"),
+            data={"zaaktypesShortProcess": ["http://tralala.nl"]},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_record_manager_update(self):
+        user = UserFactory.create(role__can_start_destruction=True)
+
+        self.client.force_login(user)
+        response = self.client.put(
+            reverse("api:archive-config"),
+            data={"zaaktypesShortProcess": ["http://tralala.nl"]},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        config = ArchiveConfig.get_solo()
+
+        self.assertEqual(config.zaaktypes_short_process, ["http://tralala.nl"])
+
+    def test_record_manager_partial_update(self):
+        user = UserFactory.create(role__can_start_destruction=True)
+
+        self.client.force_login(user)
+        response = self.client.patch(
+            reverse("api:archive-config"),
+            data={"zaaktypesShortProcess": ["http://tralala.nl"]},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        config = ArchiveConfig.get_solo()
+
+        self.assertEqual(config.zaaktypes_short_process, ["http://tralala.nl"])
