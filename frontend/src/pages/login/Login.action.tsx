cd845dfbb78f03d13d0141cb8b402931955f5f31
Sven van de Scheur <svenvandescheur@gmail.com>
2024-07-16 16:21:41 +0200

    :bug: #95 - fix: fix a bug that could cause an error message not to be shown when loggin in

--- frontend/src/pages/login/Login.action.tsx
+++ frontend/src/pages/login/Login.action.tsx
@@ -1,7 +1,8 @@
 import { ActionFunctionArgs } from "@remix-run/router/utils";
 import { redirect } from "react-router-dom";
 
-import { login, logout } from "../../lib/api/auth";
+import { login } from "../../lib/api/auth";
+import { cacheDelete } from "../../lib/cache/cache";
 import "./Login.css";
 
 /**
@@ -14,7 +15,7 @@ export async function loginAction({ request }: ActionFunctionArgs) {
   const password = formData.get("password");
 
   try {
-    await logout();
+    await cacheDelete("whoAmI");
     await login(username as string, password as string);
     const url = new URL(request.url);
     const next = url.searchParams.get("next") || "/";
