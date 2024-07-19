63bad239bf6027d01b4865267ddb7734333a6e57
Julian Roeland <julianroeland@gmail.com>
2024-07-19 14:19:55 +0200

    :art: - fix: fixed primary button

--- frontend/src/pages/destructionlist/create/components/DestructionList/DestructionList.tsx
+++ frontend/src/pages/destructionlist/create/components/DestructionList/DestructionList.tsx
@@ -56,6 +56,7 @@ export function DestructionList({
             children: labelAction,
             onClick: onSubmitSelection,
             wrap: false,
+            variant: "primary",
           },
         ],
       }}
