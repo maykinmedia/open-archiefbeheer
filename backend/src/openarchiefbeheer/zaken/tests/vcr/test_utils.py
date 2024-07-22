9d1094838078b82ad667ca219b5d3a188e5d1a55
SilviaAmAm <silvia@maykinmedia.nl>
2024-07-22 11:11:43 +0200

    :white_check_mark: [#165] Add test about deleting zaken with relations

--- backend/src/openarchiefbeheer/zaken/tests/vcr/test_utils.py
+++ backend/src/openarchiefbeheer/zaken/tests/vcr/test_utils.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, tag
 
 from vcr.unittest import VCRMixin
+from zgw_consumers.client import build_client
 from zgw_consumers.constants import APITypes
+from zgw_consumers.models import Service
 from zgw_consumers.test.factories import ServiceFactory
 
 from openarchiefbeheer.destruction.tests.factories import DestructionListItemFactory
@@ -12,6 +14,7 @@ from ...tasks import retrieve_and_cache_zaken_from_openzaak
 from ...utils import delete_zaak_and_related_objects
 
 
+@tag("vcr")
 class DeletingZakenTests(VCRMixin, TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -41,16 +44,47 @@ class DeletingZakenTests(VCRMixin, TestCase):
 
         retrieve_and_cache_zaken_from_openzaak()
 
-    def test_deleting_zaak_without_besluiten(self):
-        zaak = Zaak.objects.get(identificatie="ZAAK_4")
+    def test_delete_zaak_related_to_besluit_related_to_document(self):
+        zaak = Zaak.objects.get(identificatie="ZAAK-00")
         destruction_list_item = DestructionListItemFactory.create(zaak=zaak.url)
         result_store = ResultStore(store=destruction_list_item)
 
         delete_zaak_and_related_objects(zaak, result_store)
 
-    def test_deleting_zaak_with_document_related_to_other_besluit(self):
-        zaak = Zaak.objects.get(identificatie="ZAAK_5")
-        destruction_list_item = DestructionListItemFactory.create(zaak=zaak.url)
-        result_store = ResultStore(store=destruction_list_item)
+        zrc_service = Service.objects.get(api_type=APITypes.zrc)
+        zrc_client = build_client(zrc_service)
 
-        delete_zaak_and_related_objects(zaak, result_store)
+        with zrc_client:
+            response = zrc_client.get("zaken", headers={"Accept-Crs": "EPSG:4326"})
+            response.raise_for_status()
+
+            data = response.json()
+
+            self.assertEqual(data["count"], 1)
+            self.assertEqual(data["results"][0]["identificatie"], "ZAAK-01")
+
+        drc_service = Service.objects.get(api_type=APITypes.drc)
+        drc_client = build_client(drc_service)
+        with drc_client:
+            response = drc_client.get("enkelvoudiginformatieobjecten")
+            response.raise_for_status()
+
+            data = response.json()
+
+            self.assertEqual(data["count"], 2)
+
+            identificaties = [item["identificatie"] for item in data["results"]]
+
+            self.assertIn("DOCUMENT-01", identificaties)
+            self.assertIn("DOCUMENT-03", identificaties)
+
+        brc_service = Service.objects.get(api_type=APITypes.brc)
+        brc_client = build_client(brc_service)
+        with brc_client:
+            response = brc_client.get("besluiten")
+            response.raise_for_status()
+
+            data = response.json()
+
+            self.assertEqual(data["count"], 1)
+            self.assertEqual(data["results"][0]["identificatie"], "BESLUIT-02")
