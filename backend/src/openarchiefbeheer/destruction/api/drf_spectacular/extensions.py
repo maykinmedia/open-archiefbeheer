a770551d7fbe64368ed435972fc922e33473026c
SilviaAmAm <silvia@maykinmedia.nl>
2024-08-16 14:52:40 +0200

    :memo: [#278] Update API schema

--- backend/src/openarchiefbeheer/destruction/api/drf_spectacular/extensions.py
+++ backend/src/openarchiefbeheer/destruction/api/drf_spectacular/extensions.py
@@ -1,33 +1,45 @@
 from drf_spectacular.contrib.django_filters import DjangoFilterExtension
 from drf_spectacular.drainage import add_trace_message
 
-from openarchiefbeheer.zaken.api.filtersets import ZaakFilter
-from openarchiefbeheer.zaken.models import Zaak
 
-
-class DestructionListItemFilterExtension(DjangoFilterExtension):
-    target_class = (
-        "openarchiefbeheer.destruction.api.backends.DestructionListItemFilterBackend"
-    )
+class NestedFilterExtension(DjangoFilterExtension):
+    target_class = "openarchiefbeheer.destruction.api.backends.NestedFilterBackend"
     priority = 1
 
-    def get_schema_operation_parameters(self, auto_schema, *args, **kwargs) -> list:
-        destruction_list_items_filters = super().get_schema_operation_parameters(
-            auto_schema, *args, **kwargs
+    def get_nested_filters(self, auto_schema):
+        nested_filterset_class = self.target.get_nested_filterset_class(
+            auto_schema.view
         )
+        model = nested_filterset_class._meta.model
 
-        # Add the filters from the Zaak filter
-        filterset_class = ZaakFilter
+        result = []
+        with add_trace_message(nested_filterset_class):
+            for field_name, filter_field in nested_filterset_class.base_filters.items():
+                result += self.resolve_filter_field(
+                    auto_schema, model, nested_filterset_class, field_name, filter_field
+                )
+        return result
 
-        zaak_filters = []
+    def get_main_filters(self, auto_schema):
+        # We need to override the parent `get_schema_operation_parameters`
+        # because it does not support using the prefix in the field name
+        filterset_class = self.target.get_filterset_class(auto_schema.view)
+        model = filterset_class._meta.model
+        prefix = auto_schema.view.filterset_kwargs.get("prefix", "")
+
+        result = []
         with add_trace_message(filterset_class):
             for field_name, filter_field in filterset_class.base_filters.items():
-                zaak_filters += self.resolve_filter_field(
+                result += self.resolve_filter_field(
                     auto_schema,
-                    Zaak,
+                    model,
                     filterset_class,
-                    "zaak__" + field_name,
+                    f"{prefix}-{field_name}",
                     filter_field,
                 )
+        return result
 
-        return destruction_list_items_filters + zaak_filters
+    def get_schema_operation_parameters(self, auto_schema, *args, **kwargs) -> list:
+        main_filters = self.get_main_filters(auto_schema)
+        nested_filters = self.get_nested_filters(auto_schema)
+        return main_filters + nested_filters
