406c0d709f14b24fd49eb5fd30333e144b6e1fc7
SilviaAmAm <silvia@maykinmedia.nl>
2024-08-19 12:24:14 +0200

    :white_check_mark: [#234] Test retrieving zaaktype choices for list

--- backend/src/openarchiefbeheer/zaken/tests/test_views.py
+++ backend/src/openarchiefbeheer/zaken/tests/test_views.py
@@ -11,6 +11,11 @@ from zgw_consumers.constants import APITypes
 from zgw_consumers.test.factories import ServiceFactory
 
 from openarchiefbeheer.accounts.tests.factories import UserFactory
+from openarchiefbeheer.destruction.constants import ListItemStatus
+from openarchiefbeheer.destruction.tests.factories import (
+    DestructionListFactory,
+    DestructionListItemFactory,
+)
 
 from ..tasks import retrieve_and_cache_zaken_from_openzaak
 from .factories import ZaakFactory
@@ -171,6 +176,129 @@ class ZaaktypenChoicesViewsTestCase(APITestCase):
 
         history = m.request_history
         self.assertEqual(len(history), 1)
+
+    def test_retrieve_zaaktypen_choices_for_destruction_list(self):
+        user = UserFactory.create(role__can_start_destruction=True)
+
+        destruction_list = DestructionListFactory.create()
+        # The zaaktypen of these items should be returned,
+        # because they are in the destruction list with status 'suggested'
+        items_in_list = DestructionListItemFactory.create_batch(
+            3,
+            with_zaak=True,
+            zaak__with_expand=True,
+            destruction_list=destruction_list,
+            status=ListItemStatus.suggested,
+        )
+        # We simulate 2 items having different versions of the same zaaktype
+        items_in_list[0].zaak._expand["zaaktype"]["identificatie"] = "ZAAKTYPE-1"
+        items_in_list[0].zaak.save()
+        items_in_list[1].zaak._expand["zaaktype"]["identificatie"] = "ZAAKTYPE-1"
+        items_in_list[1].zaak.save()
+        items_in_list[2].zaak._expand["zaaktype"]["identificatie"] = "ZAAKTYPE-2"
+        items_in_list[2].zaak.save()
+
+        # This zaaktype should NOT be returned because it has status 'removed'
+        DestructionListItemFactory.create(
+            with_zaak=True,
+            destruction_list=destruction_list,
+            status=ListItemStatus.removed,
+        )
+        # These zaaktypen should NOT be returned because it is not related to the list
+        DestructionListItemFactory.create_batch(2, with_zaak=True)
+
+        self.client.force_authenticate(user=user)
+        endpoint = furl(reverse("api:retrieve-zaaktypen-choices"))
+        endpoint.args["destruction_list"] = destruction_list.uuid
+
+        response = self.client.get(endpoint.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        choices = sorted(response.json(), key=lambda choice: choice["label"])
+
+        self.assertEqual(len(choices), 2)
+        self.assertEqual(choices[0]["label"], "ZAAKTYPE-1")
+        self.assertEqual(choices[1]["label"], "ZAAKTYPE-2")
+
+        values = choices[0]["value"].split(",")
+
+        self.assertEqual(len(values), 2)
+        self.assertIn(items_in_list[0].zaak.zaaktype, values)
+        self.assertIn(items_in_list[1].zaak.zaaktype, values)
+
+        self.assertEqual(choices[1]["value"], items_in_list[2].zaak.zaaktype)
+
+    @Mocker()
+    def test_not_cached_if_query_param_chages(self, m):
+        user = UserFactory.create(role__can_start_destruction=True)
+
+        destruction_list = DestructionListFactory.create()
+
+        items_in_list = DestructionListItemFactory.create_batch(
+            2,
+            with_zaak=True,
+            zaak__with_expand=True,
+            destruction_list=destruction_list,
+            status=ListItemStatus.suggested,
+        )
+        items_in_list[0].zaak._expand["zaaktype"]["identificatie"] = "ZAAKTYPE-1"
+        items_in_list[0].zaak.save()
+        items_in_list[1].zaak._expand["zaaktype"]["identificatie"] = "ZAAKTYPE-2"
+        items_in_list[1].zaak.save()
+
+        ServiceFactory.create(
+            api_type=APITypes.ztc,
+            api_root="http://catalogi-api.nl/catalogi/api/v1",
+        )
+        ZaakFactory.create(
+            zaaktype="http://catalogi-api.nl/catalogi/api/v1/zaakypen/111-111-111",
+            _expand={"zaaktype": {"identificatie": "ZAAKTYPE-3"}},
+        )
+        ZaakFactory.create(
+            zaaktype="http://catalogi-api.nl/catalogi/api/v1/zaakypen/222-222-222",
+            _expand={"zaaktype": {"identificatie": "ZAAKTYPE-4"}},
+        )
+        ZaakFactory.create(
+            zaaktype="http://catalogi-api.nl/catalogi/api/v1/zaakypen/333-333-333",
+            _expand={"zaaktype": {"identificatie": "ZAAKTYPE-5"}},
+        )
+
+        m.get(
+            "http://catalogi-api.nl/catalogi/api/v1/zaaktypen",
+            json={
+                "count": 2,
+                "results": [
+                    {
+                        "url": "http://catalogi-api.nl/catalogi/api/v1/zaakypen/111-111-111",
+                        "identificatie": "ZAAKTYPE-3",
+                    },
+                    {
+                        "url": "http://catalogi-api.nl/catalogi/api/v1/zaakypen/222-222-222",
+                        "identificatie": "ZAAKTYPE-4",
+                    },
+                    {
+                        "url": "http://catalogi-api.nl/catalogi/api/v1/zaakypen/333-333-333",
+                        "identificatie": "ZAAKTYPE-5",
+                    },
+                ],
+            },
+        )
+
+        self.client.force_authenticate(user=user)
+        endpoint = furl(reverse("api:retrieve-zaaktypen-choices"))
+        endpoint.args["destruction_list"] = destruction_list.uuid
+
+        response = self.client.get(endpoint.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()), 2)
+
+        del endpoint.args["destruction_list"]
+        response = self.client.get(endpoint.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()), 3)
 
 
 class SelectielijstklasseChoicesViewTests(APITestCase):
