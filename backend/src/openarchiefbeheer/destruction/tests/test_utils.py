5d72b2893ed0f85c79801e3673ce0eb0097dd732
SilviaAmAm <silvia@maykinmedia.nl>
2024-08-22 14:51:24 +0200

    :white_check_mark: [#296] Test resync behaviour

--- backend/src/openarchiefbeheer/destruction/tests/test_utils.py
+++ backend/src/openarchiefbeheer/destruction/tests/test_utils.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 
 from openarchiefbeheer.accounts.tests.factories import UserFactory
+from openarchiefbeheer.zaken.tests.factories import ZaakFactory
 
 from ..constants import ListRole
-from ..utils import process_new_assignees
-from .factories import DestructionListAssigneeFactory, DestructionListFactory
+from ..utils import process_new_assignees, resync_items_and_zaken
+from .factories import (
+    DestructionListAssigneeFactory,
+    DestructionListFactory,
+    DestructionListItemFactory,
+)
 
 
 class UtilsTest(TestCase):
@@ -29,3 +34,34 @@ class UtilsTest(TestCase):
         self.assertEqual(2, new_assignees.count())
         self.assertEqual(users[0].pk, new_assignees[0].user.pk)
         self.assertEqual(users[1].pk, new_assignees[1].user.pk)
+
+    def test_resync_zaken(self):
+        ZaakFactory.create(url="http://zaken.nl/1")
+        ZaakFactory.create(url="http://zaken.nl/2")
+        item1 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/1")
+        item2 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/2")
+
+        resync_items_and_zaken()
+
+        item1.refresh_from_db()
+        item2.refresh_from_db()
+
+        self.assertIsNotNone(item1.zaak)
+        self.assertEqual(item1.zaak.url, "http://zaken.nl/1")
+        self.assertIsNotNone(item2.zaak)
+        self.assertEqual(item2.zaak.url, "http://zaken.nl/2")
+
+    def test_resync_zaken_missing(self):
+        ZaakFactory.create(url="http://zaken.nl/1")
+        ZaakFactory.create(url="http://zaken.nl/2")
+        item1 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/1")
+        item2 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/3")
+
+        resync_items_and_zaken()
+
+        item1.refresh_from_db()
+        item2.refresh_from_db()
+
+        self.assertIsNotNone(item1.zaak)
+        self.assertEqual(item1.zaak.url, "http://zaken.nl/1")
+        self.assertIsNone(item2.zaak)
