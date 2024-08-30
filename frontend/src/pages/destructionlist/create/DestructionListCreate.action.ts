528d391b9e21d138b440c099e75fb5a6fab0730a
Julian Roeland <julianroeland@gmail.com>
2024-08-30 12:04:16 +0200

    :art: - refactor: clean-up api lib with the import

--- frontend/src/pages/destructionlist/create/DestructionListCreate.action.ts
+++ frontend/src/pages/destructionlist/create/DestructionListCreate.action.ts
@@ -2,7 +2,10 @@ import { ActionFunctionArgs } from "@remix-run/router/utils";
 import { redirect } from "react-router-dom";
 
 import { createDestructionList } from "../../../lib/api/destructionLists";
-import { clearZaakSelection } from "../../../lib/zaakSelection/zaakSelection";
+import {
+  clearZaakSelection,
+  getAllZakenSelected,
+} from "../../../lib/zaakSelection/zaakSelection";
 import { DESTRUCTION_LIST_CREATE_KEY } from "./DestructionListCreate";
 
 /**
@@ -17,9 +20,18 @@ export async function destructionListCreateAction({
   const zaakUrls = formData.getAll("zaakUrls") as string[];
   const assigneeIds = formData.getAll("assigneeIds") as string[];
   const zaakFilters = formData.get("zaakFilters") as string;
+  const allZakenSelected = await getAllZakenSelected(
+    DESTRUCTION_LIST_CREATE_KEY,
+  );
 
   try {
-    await createDestructionList(name, zaakUrls, assigneeIds, zaakFilters);
+    await createDestructionList(
+      name,
+      zaakUrls,
+      assigneeIds,
+      zaakFilters,
+      allZakenSelected,
+    );
   } catch (e: unknown) {
     return await (e as Response).json();
   }
