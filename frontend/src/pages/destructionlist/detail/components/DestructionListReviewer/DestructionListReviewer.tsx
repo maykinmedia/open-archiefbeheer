b63c9e402151f0c30cc0cdfdf88f736e966f3d90
SilviaAmAm <silvia@maykinmedia.nl>
2024-09-05 10:12:57 +0200

    :construction: [#317] Remove second reviewer from the detail page

---
+++ frontend/src/pages/destructionlist/detail/components/DestructionListReviewer/DestructionListReviewer.tsx
@@ -0,0 +1,128 @@
+import {
+  AttributeTable,
+  Body,
+  Button,
+  Form,
+  Modal,
+  SerializedFormData,
+  Solid,
+} from "@maykin-ui/admin-ui";
+import { FormEvent, useState } from "react";
+import { useNavigation } from "react-router-dom";
+
+import { useSubmitAction } from "../../../../../hooks";
+import { User } from "../../../../../lib/api/auth";
+import { DestructionListRead } from "../../../../../lib/api/destructionLists";
+import { formatUser } from "../../../../../lib/format/user";
+
+export type DestructionListReviewerProps = {
+  destructionList: DestructionListRead;
+  reviewers: User[];
+};
+
+/**
+ * Allows viewing/assigning the reviewers of a destruction list.
+ * @param destructionList
+ * @param reviewers
+ */
+export function DestructionListReviewer({
+  destructionList,
+  reviewers,
+}: DestructionListReviewerProps) {
+  const { state } = useNavigation();
+  const submitAction = useSubmitAction();
+  const [modalState, setModalState] = useState<boolean>(false);
+
+  /**
+   * Gets called when the change is confirmed.
+   * @param _
+   * @param data
+   */
+  const handleSubmit = (
+    _: FormEvent,
+    { reviewer, comment }: SerializedFormData,
+  ) => {
+    if (!modalState) {
+      return;
+    }
+    submitAction({
+      type: "UPDATE_REVIEWER",
+      payload: {
+        assignee: { user: Number(reviewer) },
+        comment: String(comment),
+      },
+    });
+    setModalState(false);
+  };
+
+  const reviewer = destructionList.assignees.find(
+    (assignee) => assignee.role === "reviewer",
+  );
+
+  return (
+    <>
+      {reviewer && (
+        <>
+          <AttributeTable
+            labeledObject={{
+              reviewer: {
+                label: "Beoordelaar",
+                value: formatUser(reviewer.user),
+              },
+            }}
+          />
+
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              setModalState(true);
+            }}
+            disabled={state === "loading" || state === "submitting"}
+            size="xs"
+          >
+            <Solid.PencilIcon />
+            Bewerken
+          </Button>
+        </>
+      )}
+
+      {modalState && (
+        <Modal
+          open={modalState}
+          size="m"
+          title="Beoordelaar wijzigen"
+          onClose={() => setModalState(false)}
+        >
+          <Body>
+            <Form
+              autoComplete="off"
+              fields={[
+                {
+                  label: "Beoordelaar",
+                  name: "reviewer",
+                  type: "string",
+                  options: reviewers.map((user) => ({
+                    label: formatUser(user),
+                    value: user.pk,
+                  })),
+                  required: true,
+                },
+                {
+                  autoFocus: true,
+                  label: "Reden",
+                  name: "comment",
+                  type: "text",
+                  required: true,
+                },
+              ]}
+              initialValues={{ reviewer: reviewer?.user?.pk }}
+              validateOnChange={true}
+              onSubmit={handleSubmit}
+              labelSubmit={"Toewijzen"}
+            />
+          </Body>
+        </Modal>
+      )}
+    </>
+  );
+}
