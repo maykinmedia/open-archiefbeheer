105c247d59981878c71fdd94385869dda143c386
SilviaAmAm <silvia@maykinmedia.nl>
2024-10-01 10:23:16 +0200

    :white_check_mark: [#390] Test migrations

--- backend/src/openarchiefbeheer/accounts/tests/factories.py
+++ backend/src/openarchiefbeheer/accounts/tests/factories.py
@@ -3,16 +3,7 @@ from django.contrib.auth import get_user_model
 import factory
 from factory.django import DjangoModelFactory
 
-from ..models import Role
-
 User = get_user_model()
-
-
-class RoleFactory(DjangoModelFactory):
-    name = factory.Sequence(lambda n: f"Role {n}")
-
-    class Meta:
-        model = Role
 
 
 class UserFactory(DjangoModelFactory):
@@ -20,7 +11,6 @@ class UserFactory(DjangoModelFactory):
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     password = factory.PostGenerationMethodCall("set_password", "password")
-    role = factory.SubFactory(RoleFactory)
     email = factory.Faker("email")
 
     class Meta:
