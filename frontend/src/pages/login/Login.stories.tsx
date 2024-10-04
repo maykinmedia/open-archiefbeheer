ff4a1c33052510c3704b33ce0a1b9f62a5892ceb
SilviaAmAm <silvia@maykinmedia.nl>
2024-10-04 13:01:22 +0200

    :memo: [#390] Add story for OIDC login

--- frontend/src/pages/login/Login.stories.tsx
+++ frontend/src/pages/login/Login.stories.tsx
@@ -1,7 +1,9 @@
 import "@maykin-ui/admin-ui/style";
 import type { Meta, StoryObj } from "@storybook/react";
+import { expect, within } from "@storybook/test";
 
 import { ReactRouterDecorator } from "../../../.storybook/decorators";
+import ExtraConfigContext from "../../lib/contexts/ExtraConfigContext";
 import { LoginPage } from "./Login";
 
 const meta: Meta<typeof LoginPage> = {
@@ -16,5 +18,33 @@ type Story = StoryObj<typeof meta>;
 export const LoginPageStory: Story = {
   args: {
     children: "The quick brown fox jumps over the lazy dog.",
+  },
+};
+
+export const LoginPageWithOIDC: Story = {
+  render: (args) => (
+    <ExtraConfigContext.Provider
+      value={{
+        oidc: {
+          enabled: true,
+          loginUrl: "http://backend.nl/oidc/authenticate",
+        },
+      }}
+    >
+      <LoginPage />
+    </ExtraConfigContext.Provider>
+  ),
+  play: async (context) => {
+    const canvas = within(context.canvasElement);
+
+    const oidcButton: HTMLBaseElement = await canvas.findByRole("link", {
+      name: "Organisatie login",
+    });
+
+    const redirectUrl = new URL(oidcButton.href);
+    const nextUrl = redirectUrl.searchParams.get("next");
+
+    expect(nextUrl).not.toBeNull();
+    expect(new URL(nextUrl as string).pathname).toEqual("/");
   },
 };
