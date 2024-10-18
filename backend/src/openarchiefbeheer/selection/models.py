0c9316b022756fd182f9fdb2a32f23bd6bbc6e34
SilviaAmAm <silvia@maykinmedia.nl>
2024-10-18 15:24:20 +0200

    :construction: [#396] Selection models

---
+++ backend/src/openarchiefbeheer/selection/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class SelectionItem(models.Model):
+    key = models.CharField(
+        _("key"),
+        help_text=_(
+            "The key to access this selection. "
+            "It should be the uuid of a destruction list and an action name."
+        ),
+        max_length=250,
+    )
+    zaak_url = models.URLField(
+        _("zaak url"), help_text=_("The url of a zaak in the list."), max_length=1000
+    )
+    selection_data = models.JSONField(
+        _("selection data"),
+        help_text=_("Data about this item in the selection"),
+        default=dict,
+    )
+
+    class Meta:
+        verbose_name = _("selection item")
+        verbose_name_plural = _("selection items")
+
+
+class AllSelectedToggle(models.Model):
+    key = models.CharField(
+        _("key"),
+        help_text=_("The key to access this selection. "),
+        max_length=250,
+    )
+    all_selected = models.BooleanField(
+        _("all selected"),
+        help_text=_("Toggle to set all the items in the selection to 'selected'."),
+    )
+
+    class Meta:
+        verbose_name = _("all selected toggle")
+        verbose_name_plural = _("all selected toggles")
