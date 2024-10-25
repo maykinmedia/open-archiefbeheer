c45de8b7366989b789dba8d00b281f9aa3071791
SilviaAmAm <silvia@maykinmedia.nl>
2024-10-25 13:30:43 +0200

    :construction: [#396] Add POST endpoint

--- backend/src/openarchiefbeheer/selection/api/views.py
+++ backend/src/openarchiefbeheer/selection/api/views.py
@@ -1,12 +1,19 @@
 from django.utils.translation import gettext_lazy as _
 
-from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema
+from drf_spectacular.utils import (
+    OpenApiExample,
+    OpenApiRequest,
+    OpenApiResponse,
+    extend_schema,
+)
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import SelectionSerializer
+from .serializers import SelectionReadSerializer, SelectionWriteSerializer
 
 
+# TODO Update to use GenericAPIView?
 class SelectionView(APIView):
     @extend_schema(
         tags=["Selection"],
@@ -20,6 +27,7 @@ class SelectionView(APIView):
                     "type": "object",
                     "additionalProperties": {
                         "type": "object",
+                        "description": "The zaak URL is used as key",
                         "properties": {
                             "selected": {"type": "boolean"},
                             "details": {"type": "object"},
@@ -39,14 +47,56 @@ class SelectionView(APIView):
                                 "details": {},
                             },
                         },
-                        response_only=True,
                     ),
                 ],
             )
         },
     )
     def get(self, request, *args, **kwargs):
-        serialiser = SelectionSerializer(data=self.kwargs)
-        serialiser.is_valid()
+        serialiser = SelectionReadSerializer(data=self.kwargs)
+        serialiser.is_valid(raise_exception=True)
 
         return Response(serialiser.data)
+
+    @extend_schema(
+        tags=["Selection"],
+        summary=_("Add to selection"),
+        description=_(
+            "Get the zaken in a selection and whether they are checked or not."
+        ),
+        responses={201: None},
+        request=OpenApiRequest(
+            request={
+                "type": "object",
+                "additionalProperties": {
+                    "type": "object",
+                    "description": "The zaak URL is used as key",
+                    "properties": {
+                        "selected": {"type": "boolean"},
+                        "details": {"type": "object"},
+                    },
+                },
+            },
+            examples=[
+                OpenApiExample(
+                    "Add to the selection",
+                    value={
+                        "http://zaken.nl/api/v1/zaken/111-111-111": {
+                            "selected": True,
+                            "details": {},
+                        },
+                        "http://zaken.nl/api/v1/zaken/222-222-222": {
+                            "selected": False,
+                            "details": {},
+                        },
+                    },
+                ),
+            ],
+        ),
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = SelectionWriteSerializer(data=request.data, context=self.kwargs)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
