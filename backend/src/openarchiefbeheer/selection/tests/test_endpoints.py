3e0f2a6a31dfcc8b29c0364b546546c38cb05c89
SilviaAmAm <silvia@maykinmedia.nl>
2024-10-28 12:13:34 +0100

    :white_check_mark: [#396] Improved tests

--- backend/src/openarchiefbeheer/selection/tests/test_endpoints.py
+++ backend/src/openarchiefbeheer/selection/tests/test_endpoints.py
@@ -1,4 +1,6 @@
+from furl import furl
 from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 from openarchiefbeheer.accounts.tests.factories import UserFactory
@@ -34,7 +36,7 @@ class SelectionAPITests(APITestCase):
 
         self.client.force_login(self.user)
 
-        response = self.client.get(f"/api/v1/selection/{key}")
+        response = self.client.get(reverse("api:selections", args=[key]))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -43,6 +45,85 @@ class SelectionAPITests(APITestCase):
         self.assertFalse(data["http://zaken.nl/api/v1/zaken/111-111-111"]["selected"])
         self.assertTrue(data["http://zaken.nl/api/v1/zaken/222-222-222"]["selected"])
         self.assertFalse(data["http://zaken.nl/api/v1/zaken/333-333-333"]["selected"])
+
+    def test_create_zaak_selection(self):
+        key = "some-key"
+
+        data = {
+            "http://zaken.nl/api/v1/zaken/111-111-111": {"selected": True},
+            "http://zaken.nl/api/v1/zaken/222-222-222": {"selected": False},
+            "http://zaken.nl/api/v1/zaken/333-333-333": {
+                "selected": False,
+                "details": {"bla": "blo"},
+            },
+        }
+
+        self.client.force_login(self.user)
+
+        response = self.client.post(
+            reverse("api:selections", args=[key]),
+            data=data,
+            format="json",
+        )
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        items = SelectionItem.objects.filter(key=key)
+
+        self.assertEqual(items.count(), 3)
+
+    def test_full_update_zaak_selection(self):
+        key = "some-key"
+
+        SelectionItemFactory.create(
+            key=key,
+            is_selected=False,
+            zaak_url="http://zaken.nl/api/v1/zaken/111-111-111",
+        )
+        SelectionItemFactory.create(
+            key=key,
+            is_selected=True,
+            zaak_url="http://zaken.nl/api/v1/zaken/222-222-222",
+        )
+        SelectionItemFactory.create(
+            key=key,
+            is_selected=False,
+            zaak_url="http://zaken.nl/api/v1/zaken/333-333-333",
+        )
+
+        data = {
+            "http://zaken.nl/api/v1/zaken/111-111-111": {"selected": True},
+            "http://zaken.nl/api/v1/zaken/222-222-222": {"selected": True},
+            "http://zaken.nl/api/v1/zaken/333-333-333": {
+                "selected": False,
+                "details": {"bla": "Changed details!"},
+            },
+        }
+
+        self.client.force_login(self.user)
+
+        response = self.client.put(
+            reverse("api:selections", args=[key]),
+            data=data,
+            format="json",
+        )
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        items = SelectionItem.objects.filter(key=key)
+
+        self.assertEqual(items.count(), 3)
+        self.assertTrue(
+            items.get(
+                zaak_url="http://zaken.nl/api/v1/zaken/111-111-111"
+            ).selection_data["selected"]
+        )
+        self.assertEqual(
+            items.get(
+                zaak_url="http://zaken.nl/api/v1/zaken/333-333-333"
+            ).selection_data["details"],
+            {"bla": "Changed details!"},
+        )
 
     def test_set_zaak_selection(self):
         key = "some-key"
@@ -66,7 +147,7 @@ class SelectionAPITests(APITestCase):
         self.client.force_login(self.user)
 
         response = self.client.patch(
-            f"/api/v1/selection/{key}",
+            reverse("api:selections", args=[key]),
             data={
                 "http://zaken.nl/api/v1/zaken/111-111-111": {"selected": True},
                 "http://zaken.nl/api/v1/zaken/222-222-222": {"selected": False},
@@ -106,7 +187,7 @@ class SelectionAPITests(APITestCase):
         )
 
         self.client.force_login(self.user)
-        response = self.client.delete(f"/api/v1/selection/{key}")
+        response = self.client.delete(reverse("api:selections", args=[key]))
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
@@ -137,9 +218,11 @@ class SelectionAPITests(APITestCase):
         item3.selection_data["details"] = {"test": "tralala"}
 
         self.client.force_login(self.user)
-        response = self.client.get(
-            f"/api/v1/selection/{key}?selected=True&test=tralala"
-        )
+        endpoint = furl(reverse("api:selections", args=[key]))
+        endpoint.args["selected"] = True
+        endpoint.args["test"] = "tralala"
+
+        response = self.client.get(endpoint.url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -168,9 +251,9 @@ class SelectionAPITests(APITestCase):
         )
 
         self.client.force_login(self.user)
-        response = self.client.get(
-            f"/api/v1/selection/{key}?item=http://zaken.nl/api/v1/zaken/111-111-111"
-        )
+        endpoint = furl(reverse("api:selections", args=[key]))
+        endpoint.args["item"] = "http://zaken.nl/api/v1/zaken/111-111-111"
+        response = self.client.get(endpoint.url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
