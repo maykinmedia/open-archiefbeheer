c909898689867e8cbce5e30bb337aff9b4105928
SilviaAmAm <silvia@maykinmedia.nl>
2024-11-05 11:46:01 +0100

    :recycle: [#396] Refactor items filter

--- backend/src/openarchiefbeheer/selection/api/filtersets.py
+++ backend/src/openarchiefbeheer/selection/api/filtersets.py
@@ -1,13 +1,13 @@
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 
-from django_filters import BaseCSVFilter, BooleanFilter, CharFilter, FilterSet
+from django_filters import BaseInFilter, BooleanFilter, CharFilter, FilterSet
 from django_filters.rest_framework import DjangoFilterBackend
 
 from ..models import SelectionItem
 
 
-class CharCsvFilter(BaseCSVFilter, CharFilter):
+class CharInFilter(BaseInFilter, CharFilter):
     pass
 
 
@@ -17,9 +17,9 @@ class SelectionItemFilterset(FilterSet):
         method="filter_selected",
         help_text=_("Filter on selected items."),
     )
-    items = CharCsvFilter(
-        field_name="items",
-        method="filter_items",
+    items = CharInFilter(
+        field_name="zaak_url",
+        lookup_expr="in",
         help_text=_("Filter on the zaak url of one of more items separated by commas"),
     )
 
@@ -31,11 +31,6 @@ class SelectionItemFilterset(FilterSet):
         self, queryset: QuerySet[SelectionItem], name: str, value: bool
     ) -> QuerySet[SelectionItem]:
         return queryset.filter(selection_data__selected=value)
-
-    def filter_items(
-        self, queryset: QuerySet[SelectionItem], name: str, value: list[str]
-    ) -> QuerySet[SelectionItem]:
-        return queryset.filter(zaak_url__in=value)
 
     def make_dynamic_filters(self, request, queryset, view):
         for field_name in self.get_fields():
