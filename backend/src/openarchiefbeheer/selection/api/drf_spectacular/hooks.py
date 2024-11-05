466181b97adcc9fd7146b34a045a04c99b84ecff
SilviaAmAm <silvia@maykinmedia.nl>
2024-11-05 12:23:47 +0100

    :memo: [#396] Improve API documentation for count endpoint

--- backend/src/openarchiefbeheer/selection/api/drf_spectacular/hooks.py
+++ backend/src/openarchiefbeheer/selection/api/drf_spectacular/hooks.py
@@ -1,5 +1,11 @@
 from drf_spectacular.generators import SchemaGenerator
-from drf_spectacular.plumbing import build_examples_list, force_instance
+from drf_spectacular.plumbing import (
+    OpenApiTypes,
+    build_basic_type,
+    build_examples_list,
+    build_object_type,
+    force_instance,
+)
 from rest_framework.request import Request
 
 from ..schemas import SCHEMA_RESPONSE
@@ -9,10 +15,17 @@ def update_schema_for_dynamic_keys(
     result: dict, generator: SchemaGenerator, request: Request, public: bool
 ) -> dict:
     openapi_response_schema = force_instance(SCHEMA_RESPONSE)
+
     result["paths"]["/api/v1/selections/{key}/"]["get"]["responses"]["200"]["content"][
         "application/json"
     ]["schema"] = openapi_response_schema.response
     result["paths"]["/api/v1/selections/{key}/"]["get"]["responses"]["200"]["content"][
         "application/json"
     ]["examples"] = build_examples_list(openapi_response_schema.examples)
+
+    result["paths"]["/api/v1/selections/{key}/count/"]["get"]["responses"]["200"][
+        "content"
+    ]["application/json"]["schema"] = build_object_type(
+        {"count": build_basic_type(OpenApiTypes.INT)}
+    )
     return result
