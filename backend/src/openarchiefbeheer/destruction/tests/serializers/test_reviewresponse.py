f339c2f66835f81f60862b9d1d2c8ab190618070
SilviaAmAm <silvia@maykinmedia.nl>
2024-11-15 11:50:33 +0100

    :test_tube: [#416] Test empty selectielijstklasse on zaak

--- backend/src/openarchiefbeheer/destruction/tests/serializers/test_reviewresponse.py
+++ backend/src/openarchiefbeheer/destruction/tests/serializers/test_reviewresponse.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from django.test import TestCase
+from django.test import TestCase, tag
 from django.utils.translation import gettext_lazy as _
 
 from requests.exceptions import HTTPError
@@ -178,3 +178,41 @@ class ReviewResponseSerializerTests(TestCase):
                 "Could not validate the selectielijstklasse waardering due to an unexpected response from the Selectielijst API."
             ),
         )
+
+    @tag("gh-416")
+    def test_update_archiefactiedatum_when_selectielijstklasse_is_empty(self):
+        record_manager = UserFactory.create(post__can_start_destruction=True)
+        review = DestructionListReviewFactory.create(
+            destruction_list__author=record_manager,
+            destruction_list__status=ListStatus.changes_requested,
+            destruction_list__assignee=record_manager,
+        )
+        items_review = DestructionListItemReviewFactory.create(
+            destruction_list_item__with_zaak=True,
+            destruction_list_item__zaak__selectielijstklasse=None,
+            destruction_list_item__destruction_list=review.destruction_list,
+            review=review,
+        )
+
+        data = {
+            "review": review.pk,
+            "comment": "A response about the review.",
+            "items_responses": [
+                {
+                    "review_item": items_review.pk,
+                    "action_item": DestructionListItemAction.remove,
+                    "action_zaak_type": ZaakActionType.bewaartermijn,
+                    "action_zaak": {
+                        "archiefactiedatum": "2030-01-01",
+                    },
+                    "comment": "This zaak needs to kept.",
+                },
+            ],
+        }
+
+        request = factory.get("/foo")
+        request.user = record_manager
+
+        serializer = ReviewResponseSerializer(data=data, context={"request": request})
+
+        self.assertTrue(serializer.is_valid())
