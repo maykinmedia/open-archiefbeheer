d63329557dfa0da452b9b0464861af0f3f103c4b
SilviaAmAm <silvia@maykinmedia.nl>
2024-11-25 16:59:26 +0100

    :sparkles: [#499] Add log filtering for event

--- backend/src/openarchiefbeheer/logging/api/filtersets.py
+++ backend/src/openarchiefbeheer/logging/api/filtersets.py
@@ -2,7 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 
-from django_filters import FilterSet, UUIDFilter
+from django_filters import CharFilter, FilterSet, UUIDFilter
 from timeline_logger.models import TimelineLog
 
 from openarchiefbeheer.destruction.models import DestructionList
@@ -13,6 +13,11 @@ class LogsFilterset(FilterSet):
         field_name="destruction_list",
         method="filter_destruction_list",
         help_text=_("Retrieve the logs of a particular destruction list."),
+    )
+    event = CharFilter(
+        field_name="event",
+        method="filter_event",
+        help_text=_("Retrieve the logs for a particular event."),
     )
 
     class Meta:
@@ -32,3 +37,9 @@ class LogsFilterset(FilterSet):
             content_type=content_type,
             object_id=destruction_list.pk,
         )
+
+    def filter_event(
+        self, queryset: QuerySet[TimelineLog], name: str, value: str
+    ) -> QuerySet[TimelineLog]:
+        template = f"logging/{value}.txt"
+        return queryset.filter(template=template)
