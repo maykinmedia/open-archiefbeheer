95973cd9aca8650ec0b13f3200751600b2dcb633
SilviaAmAm <silvia@maykinmedia.nl>
2024-12-05 15:24:36 +0100

    :hammer: [#458] Add timeout to check if migrations are done

--- backend/bin/setup_configuration.sh
+++ backend/bin/setup_configuration.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+# Waiting for database to be up
 until pg_isready; do
   >&2 echo "Waiting for database connection..."
   sleep 1
 done
 
-until python src/manage.py migrate --check; do
-  >&2 echo "Waiting for migrations to be done..."
-  sleep 1
+# Waiting for migrations to be done
+attempt_counter=0
+max_attempts=${CHECK_MIGRATIONS_MAX_ATTEMPT:-10}
+
+while ! python src/manage.py migrate --check; do
+  attempt_counter=$((attempt_counter + 1))
+
+  if [ $attempt_counter -ge $max_attempts ]; then
+    >&2 echo "Timed out while waiting for django migrations."
+    exit 1
+  fi
+
+  >&2 echo "Attempt $attempt_counter/$max_attempts: Waiting for migrations to be done..."
+  sleep 10
 done
 
 # Run setup configuration
