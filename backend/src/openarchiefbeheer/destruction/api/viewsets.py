d4fc0dba08992efd3dbf9c3d5998adb428f6d10b
SilviaAmAm <silvia@maykinmedia.nl>
2024-12-24 15:18:47 +0100

    :memo: [#557] Update API Schema

--- backend/src/openarchiefbeheer/destruction/api/viewsets.py
+++ backend/src/openarchiefbeheer/destruction/api/viewsets.py
@@ -223,6 +223,12 @@ from .serializers import (
         request=AbortDestructionSerializer,
         responses={200: None},
     ),
+    download_report=extend_schema(
+        tags=["Destruction list"],
+        summary=_("Download destruction report"),
+        request=None,
+        responses={200: None},
+    ),
 )
 class DestructionListViewSet(
     mixins.RetrieveModelMixin,
