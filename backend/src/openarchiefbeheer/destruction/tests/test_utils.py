20eabd20cd1f1c7d0abc4777a0ec1cf038b32306
SilviaAmAm <silvia@maykinmedia.nl>
2025-01-07 11:59:09 +0100

    :white_check_mark: [#554] Improve test

--- backend/src/openarchiefbeheer/destruction/tests/test_utils.py
+++ backend/src/openarchiefbeheer/destruction/tests/test_utils.py
@@ -1,9 +1,13 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
+
+from timeline_logger.models import TimelineLog
 
 from openarchiefbeheer.accounts.tests.factories import UserFactory
 from openarchiefbeheer.zaken.tests.factories import ZaakFactory
 
 from ..constants import ListRole
+from ..models import DestructionListItem
 from ..utils import process_new_reviewer, resync_items_and_zaken
 from .factories import (
     DestructionListAssigneeFactory,
@@ -48,12 +52,20 @@ class UtilsTest(TestCase):
         ZaakFactory.create(url="http://zaken.nl/2")
         item1 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/1")
         item2 = DestructionListItemFactory.create(_zaak_url="http://zaken.nl/3")
+        item2_pk = item2.pk
+        destruction_list2 = item2.destruction_list
 
         resync_items_and_zaken()
 
         item1.refresh_from_db()
-        item2.refresh_from_db()
 
         self.assertIsNotNone(item1.zaak)
         self.assertEqual(item1.zaak.url, "http://zaken.nl/1")
-        self.assertIsNone(item2.zaak)
+        with self.assertRaises(ObjectDoesNotExist):
+            DestructionListItem.objects.get(pk=item2_pk)
+
+        logs = TimelineLog.objects.for_object(destruction_list2)
+
+        self.assertEqual(logs.count(), 1)
+        self.assertEqual(logs[0].extra_data["number_deleted_items"], 1)
+        self.assertEqual(logs[0].extra_data["number_of_zaken"], 0)
