e2e909a2bf2fab92072cf1b5901f57458e20a3a1
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-09 15:47:55 +0100

    :lipstick: #584 - fix: fix incorrect styling of the landing page toolbar

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -238,6 +238,7 @@ export const Landing = () => {
         },
         objectLists: objectLists,
         toolbarProps: {
+          variant: "normal",
           items: [
             {
               icon: <Outline.MagnifyingGlassIcon />,
