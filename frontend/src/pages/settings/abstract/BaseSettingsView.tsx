372bada956d385d75bdf5355e77fdc2758b39334
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-10 10:46:06 +0100

    :sparkles: #553 - feat: add sidebar to BaseSettingsView

--- frontend/src/pages/settings/abstract/BaseSettingsView.tsx
+++ frontend/src/pages/settings/abstract/BaseSettingsView.tsx
@@ -1,5 +1,6 @@
 import { DataGridProps, ListTemplate, ToolbarItem } from "@maykin-ui/admin-ui";
 import React from "react";
+import { useMatches, useNavigate } from "react-router-dom";
 
 export type BaseSettingsPageProps<T extends object> = React.PropsWithChildren<{
   dataGridProps: DataGridProps<T>;
@@ -14,9 +15,21 @@ export function BaseSettingsView<T extends object>({
   dataGridProps,
   secondaryNavigationItems,
 }: BaseSettingsPageProps<T>) {
+  const navigate = useNavigate();
+  const currentMatch = [...useMatches()].pop(); // Explicit clone.
+  const currentPathName = currentMatch?.pathname;
+
   return (
     <ListTemplate<T>
       secondaryNavigationItems={secondaryNavigationItems}
+      sidebarItems={[
+        {
+          children: "Verkorte procedure",
+          align: "start",
+          onClick: () => navigate("/settings/short-procedure"),
+          active: currentPathName === "/settings/short-procedure",
+        },
+      ]}
       dataGridProps={dataGridProps}
     >
       {children}
