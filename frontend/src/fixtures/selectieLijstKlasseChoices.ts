8453012d0d3720b308c520710093e3d5109ede55
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-13 13:05:05 +0100

    :white_check_mark: #553 - test: fix broken story for processing a review

--- frontend/src/fixtures/selectieLijstKlasseChoices.ts
+++ frontend/src/fixtures/selectieLijstKlasseChoices.ts
@@ -6,7 +6,7 @@ import { zakenFactory } from "./zaak";
 interface SelectieLijstKlasseChoice {
   label: string;
   value: string;
-  detail?: {
+  extraData?: {
     bewaartermijn: string | null;
   };
 }
@@ -16,7 +16,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.1 - Ingericht - vernietigen - P10Y",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/afa30940-855b-4a7e-aa21-9e15a8078814",
-    detail: {
+    extraData: {
       bewaartermijn: "P10Y",
     },
   },
@@ -24,7 +24,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.1.1 - Ingericht - blijvend_bewaren",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/8af64c99-a168-40dd-8afd-9fbe0597b6dc",
-    detail: {
+    extraData: {
       bewaartermijn: null,
     },
   },
@@ -32,7 +32,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.1.2 - Ingericht - blijvend_bewaren",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/e84a06ac-1bdc-4e9c-9598-a22faa562459",
-    detail: {
+    extraData: {
       bewaartermijn: null,
     },
   },
@@ -40,7 +40,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.1.3 - Ingericht - vernietigen - P10Y",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/4086fe50-c79c-4d9b-90fc-71783f01c198",
-    detail: {
+    extraData: {
       bewaartermijn: "P10Y",
     },
   },
@@ -48,7 +48,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.2 - Ingesteld - blijvend_bewaren",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/ef6ec016-7747-4e71-b62f-d33cf90e0bc7",
-    detail: {
+    extraData: {
       bewaartermijn: null,
     },
   },
@@ -56,7 +56,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.3 - Opgeheven - blijvend_bewaren",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/784745d8-74d5-466c-93ff-6c1049364cb9",
-    detail: {
+    extraData: {
       bewaartermijn: null,
     },
   },
@@ -64,7 +64,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.4 - Niet doorgegaan - vernietigen - P5Y",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/4811c2bc-3255-4cd4-a00a-7ed59223b8b1",
-    detail: {
+    extraData: {
       bewaartermijn: "P5Y",
     },
   },
@@ -72,7 +72,7 @@ const FIXTURE_SELECTIELIJSTKLASSE_CHOICES: SelectieLijstKlasseChoice[] = [
     label: "1.5 - Afgebroken - vernietigen - P1Y",
     value:
       "https://selectielijst.openzaak.nl/api/v1/resultaten/914f4198-3e73-497f-807f-1d17ee0af21f",
-    detail: {
+    extraData: {
       bewaartermijn: "P1Y",
     },
   },
