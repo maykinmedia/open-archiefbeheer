25a0c7e9c49a8ec7ab27082e3cb7391f3bc4fb17
SilviaAmAm <silvia@maykinmedia.nl>
2025-01-14 12:13:25 +0100

    :white_check_mark: [#553] Test filtering informatieobjecttype choices

--- backend/src/openarchiefbeheer/zaken/tests/vcr/test_views.py
+++ backend/src/openarchiefbeheer/zaken/tests/vcr/test_views.py
@@ -137,3 +137,66 @@ class ResultaattypenChoicesViewTests(VCRMixin, APITestCase):
             data[0]["value"],
             "http://localhost:8003/catalogi/api/v1/resultaattypen/12903100-b7a0-4441-9645-eda7df2ad106",
         )
+
+
+class InformatieobjecttypenChoicesViewTests(VCRMixin, APITestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+        cls.zrc_service = ServiceFactory.create(
+            api_type=APITypes.ztc,
+            api_root="http://localhost:8003/catalogi/api/v1",
+            client_id="test-vcr",
+            secret="test-vcr",
+        )
+
+    def setUp(self):
+        super().setUp()
+
+        self.addCleanup(cache.clear)
+        self.addCleanup(retrieve_paginated_type.cache_clear)
+
+    def test_retrieve_all_choices(self):
+        user = UserFactory.create()
+
+        self.client.force_authenticate(user=user)
+        response = self.client.get(reverse("api:retrieve-informatieobjecttype-choices"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertEqual(len(data), 2)
+
+        results = [informatieobjecttype["value"] for informatieobjecttype in data]
+
+        self.assertIn(
+            "http://localhost:8003/catalogi/api/v1/informatieobjecttypen/a8089bdf-72d3-414f-a9cd-953cfa602b6c",
+            results,
+        )
+        self.assertIn(
+            "http://localhost:8003/catalogi/api/v1/informatieobjecttypen/9dee6712-122e-464a-99a3-c16692de5485",
+            results,
+        )
+
+    def test_retrieve_choices_with_filters(self):
+        user = UserFactory.create()
+
+        endpoint = furl(reverse("api:retrieve-informatieobjecttype-choices"))
+        endpoint.args["zaaktype"] = (
+            "http://localhost:8003/catalogi/api/v1/zaaktypen/be210495-20b6-48ff-8d3d-3e44f74c43a4"
+        )
+
+        self.client.force_authenticate(user=user)
+        response = self.client.get(endpoint.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0]["value"],
+            "http://localhost:8003/catalogi/api/v1/informatieobjecttypen/a8089bdf-72d3-414f-a9cd-953cfa602b6c",
+        )
