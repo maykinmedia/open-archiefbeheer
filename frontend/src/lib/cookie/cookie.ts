22b93ee643a29c001f95ae32ea3cdeee92a36bda
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-16 10:06:27 +0100

    :white_check_mark: - test: add tests for cookie lib

--- frontend/src/lib/cookie/cookie.ts
+++ frontend/src/lib/cookie/cookie.ts
@@ -3,15 +3,13 @@
  * @param name
  */
 export function getCookie(name: string): string | null {
-  const nameEQ = name + "=";
   const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
+  for (let c of ca) {
     while (c.charAt(0) === " ") {
       c = c.substring(1);
     }
-    if (c.indexOf(nameEQ) === 0) {
-      return c.substring(nameEQ.length, c.length);
+    if (c.indexOf(name + "=") === 0) {
+      return c.substring(name.length + 1, c.length).trim();
     }
   }
   return null;
