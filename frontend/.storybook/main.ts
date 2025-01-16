90849ebde731074fa9963190fe84f20bc228ef2f
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-16 11:36:08 +0100

    :wrench: - test: remove fixture from coverage

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -4,13 +4,20 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
-    "@storybook/addon-coverage",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "storybook-addon-mock",
     "storybook-addon-module-mock",
+    {
+      name: "@storybook/addon-coverage",
+      options: {
+        istanbul: {
+          exclude: "**/fixtures/**",
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
