53e6c327fe2233af1508de51c058d07f37bd9c20
Sven van de Scheur <svenvandescheur@gmail.com>
2025-01-16 15:08:54 +0100

    :white_check_mark: #553 - test: add shortProcedureSettingsPageLoader to test

--- frontend/src/pages/settings/pages/short-procedure/ShortProcedureSettingsPage.stories.tsx
+++ frontend/src/pages/settings/pages/short-procedure/ShortProcedureSettingsPage.stories.tsx
@@ -1,31 +1,60 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { ReactRouterDecorator } from "../../../../../.storybook/decorators";
+import { MOCKS } from "../../../../../.storybook/mockData";
 import { assertCheckboxSelection } from "../../../../../.storybook/playFunctions";
+import {
+  recordManagerFactory,
+  zaaktypeChoicesFactory,
+} from "../../../../fixtures";
 import { ShortProcedureSettingsPage } from "./ShortProcedureSettingsPage";
+import {
+  ShortProcedureSettingsPageContext,
+  shortProcedureSettingsPageLoader,
+} from "./ShortProcedureSettingsPage.loader";
+
+const FIXTURE: ShortProcedureSettingsPageContext = {
+  zaaktypeChoices: [],
+  zaaktypesShortProcess: [],
+};
 
 const meta: Meta<typeof ShortProcedureSettingsPage> = {
   title: "Pages/Settings",
   component: ShortProcedureSettingsPage,
   decorators: [ReactRouterDecorator],
+  parameters: {
+    mockData: [
+      MOCKS.OIDC_INFO,
+      {
+        url: "http://localhost:8000/api/v1/whoami/",
+        method: "GET",
+        status: 200,
+        response: recordManagerFactory(),
+      },
+      {
+        url: "http://localhost:8000/api/v1/archive-config?",
+        method: "GET",
+        status: 200,
+        response: FIXTURE,
+      },
+      {
+        url: "http://localhost:8000/api/v1/_zaaktypen-choices/?notInDestructionList=true",
+        method: "GET",
+        status: 200,
+        response: zaaktypeChoicesFactory(),
+      },
+    ],
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const FIXTURE = {
-  zaaktypesShortProcess: [],
-  zaaktypeChoices: Array.from({ length: 15 }, (_, i) => ({
-    label: `Zaaktype ${i}`,
-    value: i.toString(),
-  })),
-};
-
 export const UpdateShortProcedureSettings: Story = {
   parameters: {
     reactRouterDecorator: {
       route: {
-        loader: async () => FIXTURE,
+        loader: shortProcedureSettingsPageLoader,
       },
     },
   },
