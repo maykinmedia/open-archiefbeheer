9c7f772533e7b1e4ff0cf8adad18565d5a05dfdf
Julian Roeland <julian@maykinmedia.nl>
2025-01-21 16:47:23 +0100

    :white_check_mark: - test: improve test coverage

--- frontend/src/components/DestructionListAuditLog/DestructionListAuditLogDetails.stories.tsx
+++ frontend/src/components/DestructionListAuditLog/DestructionListAuditLogDetails.stories.tsx
@@ -23,6 +23,10 @@ export const AuditLogDetails: StoryObj<typeof DestructionListAuditLogDetails> =
           maxArchiefactiedatum: "2021-12-31T00:00:00Z",
           comment: "This is a comment",
           numberOfZaken: 10,
+          zaaktypen: [
+            { label: "Zaaktype 1", value: "1" },
+            { label: "Zaaktype 2", value: "2" },
+          ],
         },
         user: userFactory({
           username: "johndoe",
@@ -44,6 +48,7 @@ export const AuditLogDetails: StoryObj<typeof DestructionListAuditLogDetails> =
       expect(
         canvas.getByText("31/12/2021", { exact: false }),
       ).toBeInTheDocument();
+      expect(canvas.getByText("Zaaktype 1, Zaaktype 2")).toBeInTheDocument();
       expect(canvas.getByText("This is a comment")).toBeInTheDocument();
       expect(canvas.getByText("10")).toBeInTheDocument();
     },
