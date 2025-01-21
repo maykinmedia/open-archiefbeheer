6eefeaa02e675e74d834f3c8d9ce960ab6927d5a
SilviaAmAm <silvia@maykinmedia.nl>
2025-01-21 12:30:53 +0100

    :zap: [#469] Reduce DB and HTTPs connections
    - Avoid retrieving the selectielijst service for each zaak
    - Take advantage of connection pooling by sharing client between zaken

--- backend/src/openarchiefbeheer/zaken/tasks.py
+++ backend/src/openarchiefbeheer/zaken/tasks.py
@@ -10,13 +10,14 @@ from zgw_consumers.constants import APITypes
 from zgw_consumers.models import Service
 
 from openarchiefbeheer.celery import app
+from openarchiefbeheer.config.models import APIConfig
 from openarchiefbeheer.destruction.utils import resync_items_and_zaken
 from openarchiefbeheer.logging import logevent
 
 from .api.serializers import ZaakSerializer
 from .decorators import log_errors
 from .models import Zaak
-from .utils import pagination_helper, process_expanded_data
+from .utils import NoClient, pagination_helper, process_expanded_data
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +26,10 @@ logger = logging.getLogger(__name__)
 def retrieve_and_cache_zaken_from_openzaak() -> None:
     zrc_service = Service.objects.get(api_type=APITypes.zrc)
     zrc_client = build_client(zrc_service)
+
+    config = APIConfig.get_solo()
+    service = config.selectielijst_api_service
+    selectielijst_api_client = build_client(service) if service else None
 
     today = datetime.date.today()
     query_params = {
@@ -53,24 +58,27 @@ def retrieve_and_cache_zaken_from_openzaak() -> None:
             timeout=settings.REQUESTS_DEFAULT_TIMEOUT,
         )
 
-    for index, data in enumerate(data_iterator):
-        logger.info("Retrieved page %s.", index + 1)
+    with selectielijst_api_client or NoClient():
+        for index, data in enumerate(data_iterator):
+            logger.info("Retrieved page %s.", index + 1)
 
-        zaken = process_expanded_data(data["results"])
+            zaken = data["results"]
+            if selectielijst_api_client:
+                zaken = process_expanded_data(zaken, selectielijst_api_client)
 
-        # Since before we indexed also zaken without einddatum, prevent
-        # crash when retrieving zaken.
-        if zaken_in_db:
-            new_zaken = {zaak["url"]: zaak for zaak in zaken}
-            duplicates = Zaak.objects.filter(url__in=new_zaken.keys())
-            if duplicates.exists():
-                for duplicate in duplicates:
-                    del new_zaken[duplicate.url]
-            zaken = [zaak for zaak in new_zaken.values()]
+            # Since before we indexed also zaken without einddatum, prevent
+            # crash when retrieving zaken.
+            if zaken_in_db:
+                new_zaken = {zaak["url"]: zaak for zaak in zaken}
+                duplicates = Zaak.objects.filter(url__in=new_zaken.keys())
+                if duplicates.exists():
+                    for duplicate in duplicates:
+                        del new_zaken[duplicate.url]
+                zaken = [zaak for zaak in new_zaken.values()]
 
-        serializer = ZaakSerializer(data=zaken, many=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+            serializer = ZaakSerializer(data=zaken, many=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
 
 @app.task
@@ -87,8 +95,12 @@ def resync_zaken():
         "einddatum__lt": today.isoformat(),
     }
 
+    config = APIConfig.get_solo()
+    service = config.selectielijst_api_service
+    selectielijst_api_client = build_client(service) if service else None
+
     logevent.resync_started()
-    with transaction.atomic(), zrc_client:
+    with transaction.atomic(), zrc_client, selectielijst_api_client or NoClient():
         Zaak.objects.all().delete()
 
         response = zrc_client.get(
@@ -109,7 +121,9 @@ def resync_zaken():
         for index, data in enumerate(data_iterator):
             logger.info("Retrieved page %s.", index + 1)
 
-            zaken = process_expanded_data(data["results"])
+            zaken = data["results"]
+            if selectielijst_api_client:
+                zaken = process_expanded_data(zaken, selectielijst_api_client)
 
             serializer = ZaakSerializer(data=zaken, many=True)
             serializer.is_valid(raise_exception=True)
