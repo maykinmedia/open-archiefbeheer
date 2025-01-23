55247575b417493fa86fa10623ef5ead61bb921b
SilviaAmAm <silvia@maykinmedia.nl>
2025-01-23 16:35:17 +0100

    :sparkles: [#638] Update admin

--- backend/src/openarchiefbeheer/emails/admin.py
+++ backend/src/openarchiefbeheer/emails/admin.py
@@ -14,22 +14,32 @@ class EmailConfigAdmin(SingletonModelAdmin):
             {
                 "fields": [
                     "subject_review_required",
-                    "body_review_required",
+                    "body_review_required_html",
+                    "body_review_required_text",
                     "subject_co_review_request",
-                    "body_co_review_request",
+                    "body_co_review_request_html",
+                    "body_co_review_request_text",
                 ],
             },
         ),
         (
             _("Template review reminder"),
             {
-                "fields": ["subject_review_reminder", "body_review_reminder"],
+                "fields": [
+                    "subject_review_reminder",
+                    "body_review_reminder_html",
+                    "body_review_reminder_text",
+                ],
             },
         ),
         (
             _("Template changes requested"),
             {
-                "fields": ["subject_changes_requested", "body_changes_requested"],
+                "fields": [
+                    "subject_changes_requested",
+                    "body_changes_requested_html",
+                    "body_changes_requested_text",
+                ],
             },
         ),
         (
@@ -37,7 +47,8 @@ class EmailConfigAdmin(SingletonModelAdmin):
             {
                 "fields": [
                     "subject_positive_review",
-                    "body_positive_review",
+                    "body_positive_review_html",
+                    "body_positive_review_text",
                 ]
             },
         ),
@@ -46,7 +57,8 @@ class EmailConfigAdmin(SingletonModelAdmin):
             {
                 "fields": [
                     "subject_error_during_deletion",
-                    "body_error_during_deletion",
+                    "body_error_during_deletion_html",
+                    "body_error_during_deletion_text",
                 ]
             },
         ),
@@ -55,7 +67,8 @@ class EmailConfigAdmin(SingletonModelAdmin):
             {
                 "fields": [
                     "subject_successful_deletion",
-                    "body_successful_deletion",
+                    "body_successful_deletion_html",
+                    "body_successful_deletion_text",
                 ]
             },
         ),
