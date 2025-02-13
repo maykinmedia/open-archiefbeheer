ebc74736efc49a76417859afcb0b79fee0741974
Sven van de Scheur <svenvandescheur@gmail.com>
2025-02-13 16:26:59 +0100

    :bug: #654 - fix: fix a bug that cause ordering button to be shown on fields that cannot be ordered

--- frontend/src/pages/destructionlist/review/DestructionListReview.tsx
+++ frontend/src/pages/destructionlist/review/DestructionListReview.tsx
@@ -489,12 +489,8 @@ export function DestructionListReviewPage() {
       paginatedZaken={paginatedObjectList}
       secondaryNavigationItems={[getSubmitDestructionListButton()]}
       extraFields={[
-        {
-          filterable: false,
-          name: "Beoordeling",
-          type: "text",
-        },
-        { filterable: false, name: "Acties", type: "text", width: "230px" },
+        { name: "Beoordeling", type: "text" },
+        { name: "Acties", type: "text", width: "230px" },
       ]}
       dataGridProps={{
         labelSelect: "Markeren als (on)gezien",
