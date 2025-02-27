af461b4dcde032bd6a1cea5b45ddaf072bf86516
SilviaAmAm <silvia@maykinmedia.nl>
2025-02-27 15:23:41 +0100

    :building_construction: [#711] Update frontend to use zaaktype identificatie

--- frontend/src/fixtures/zaakTypeChoices.ts
+++ frontend/src/fixtures/zaakTypeChoices.ts
@@ -4,40 +4,33 @@ import { createArrayFactory } from "./factory";
 
 const FIXTURE_ZAAKTYPE_CHOICE: Option = {
   label: "Aangifte behandelen 1",
-  value:
-    "http://localhost:8000/catalogi/api/v1/zaaktypen/64c98539-076e-4fbf-8fec-fa86c560fb24",
+  value: "ZAAKTYPE-01",
 };
 
 const FIXTURE_ZAAKTYPE_CHOICES: Option[] = [
   {
     label: "Aangifte behandelen 1",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/64c98539-076e-4fbf-8fec-fa86c560fb24",
+    value: "ZAAKTYPE-01",
   },
   {
     label: "Aangifte behandelen 2",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/927eb71c-d99b-4c5d-b3e2-94a07ce85923",
+    value: "ZAAKTYPE-02",
   },
   {
     label: "Aangifte behandelen 3",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/684b9c68-a36f-4c72-b044-fa9cdcb17ec9",
+    value: "ZAAKTYPE-03",
   },
   {
     label: "Aangifte behandelen 4",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/e4f2a6b0-9377-400a-b0ce-ed66c0a315da",
+    value: "ZAAKTYPE-04",
   },
   {
     label: "Aangifte behandelen 5",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/3206d651-d0f2-4690-933d-cc690444184f",
+    value: "ZAAKTYPE-05",
   },
   {
     label: "Aangifte behandelen 6",
-    value:
-      "http://localhost:8000/catalogi/api/v1/zaaktypen/773b6b77-486a-4b6c-be3e-cf13f4387cf3",
+    value: "ZAAKTYPE-06",
   },
 ];
 
