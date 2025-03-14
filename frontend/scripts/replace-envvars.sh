e9a4ed0d9591ed754510cdabee3f47286b242221
SilviaAmAm <silvia@maykinmedia.nl>
2025-03-14 15:40:02 +0100

    :truck: [#738] Rename REACT_APP_ env vars to OAB_

--- frontend/scripts/replace-envvars.sh
+++ frontend/scripts/replace-envvars.sh
@@ -4,7 +4,7 @@ set -eux -o pipefail
 
 for file in /app/static/assets/*.js;
 do
-  sed -i 's|REACT_APP_API_URL_PLACEHOLDER|'${REACT_APP_API_URL}'|g' $file
-  sed -i 's|REACT_APP_API_PATH_PLACEHOLDER|'${REACT_APP_API_PATH}'|g' $file
-  sed -i 's|REACT_APP_ZAAK_URL_TEMPLATE_PLACEHOLDER|'${REACT_APP_ZAAK_URL_TEMPLATE}'|g' $file
+  sed -i 's|OAB_API_URL_PLACEHOLDER|'${OAB_API_URL}'|g' $file
+  sed -i 's|OAB_API_PATH_PLACEHOLDER|'${OAB_API_PATH}'|g' $file
+  sed -i 's|OAB_ZAAK_URL_TEMPLATE_PLACEHOLDER|'${OAB_ZAAK_URL_TEMPLATE}'|g' $file
 done
