9af28a8db09bb81f32f27d669b0e42c7dc38548c
SilviaAmAm <silvia@maykinmedia.nl>
2025-03-17 11:51:23 +0100

    :wrench: [#730] Remove selectielijstklasse setting and make resultaattype required

--- backend/src/openarchiefbeheer/config/admin.py
+++ backend/src/openarchiefbeheer/config/admin.py
@@ -24,7 +24,6 @@ class ArchiveConfigAdmin(SingletonModelAdmin):
                     "statustype",
                     "resultaattype",
                     "informatieobjecttype",
-                    "selectielijstklasse",
                 ],
             },
         ),
