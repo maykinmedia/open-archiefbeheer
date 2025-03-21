f49ebdc93e458efc89fe2b9bcadc80935e358810
Sven van de Scheur <svenvandescheur@gmail.com>
2025-03-21 10:21:04 +0100

    :white_check_mark: - test: refactor test_login_redirects_to_destruction_list to GherkinLikeTestCase in an attempt to reduce flakiness

--- backend/src/openarchiefbeheer/destruction/tests/e2e/test_views.py
+++ backend/src/openarchiefbeheer/destruction/tests/e2e/test_views.py
@@ -1,35 +1,13 @@
 from django.test import tag
 
-from asgiref.sync import sync_to_async
-from playwright.async_api import expect
-
-from openarchiefbeheer.accounts.tests.factories import UserFactory
-from openarchiefbeheer.utils.tests.e2e import PlaywrightTestCase, browser_page
+from openarchiefbeheer.utils.tests.e2e import browser_page
+from openarchiefbeheer.utils.tests.gherkin import GherkinLikeTestCase
 
 
 @tag("e2e")
-class DestructionListViewsTests(PlaywrightTestCase):
+class DestructionListViewsTests(GherkinLikeTestCase):
     async def test_login_redirects_to_destruction_list(self):
-        # Any function interacting with the database needs to be wrapped with sync_to_async
-        @sync_to_async
-        def _create_record_manager():
-            record_manager = UserFactory.create(
-                post__can_start_destruction=True,
-                password="ANic3Password",
-            )
-            return record_manager
-
-        record_manager = await _create_record_manager()
-
         async with browser_page() as page:
-            await page.goto(self.live_server_url)
-
-            await page.wait_for_url(
-                f"{self.live_server_url}/login?next=/destruction-lists"
-            )
-
-            await page.get_by_label("Gebruikersnaam").fill(record_manager.username)
-            await page.get_by_label("Wachtwoord").fill("ANic3Password")
-            await page.get_by_role("button", name="Inloggen").click()
-
-            await expect(page).to_have_url(f"{self.live_server_url}/destruction-lists")
+            await self.given.record_manager_exists()
+            await self.when.record_manager_logs_in(page)
+            await self.then.path_should_be(page, "/destruction-lists")
