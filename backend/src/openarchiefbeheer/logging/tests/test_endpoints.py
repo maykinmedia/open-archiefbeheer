f7b0d2d242a7c0facb87b3d9d5cfffef29b58a92
Sven van de Scheur <svenvandescheur@gmail.com>
2025-04-15 12:53:21 +0200

    :bug: #817 - fix: fix default ordering in logging endpoint

--- backend/src/openarchiefbeheer/logging/tests/test_endpoints.py
+++ backend/src/openarchiefbeheer/logging/tests/test_endpoints.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 from furl import furl
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -49,3 +52,48 @@ class LogsViewsetTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 1)
+
+    def test_order(self):
+        record_manager = UserFactory.create(post__can_start_destruction=True)
+        destruction_list = DestructionListFactory.create()
+        today = datetime.now().replace(tzinfo=ZoneInfo("Europe/Amsterdam"))
+
+        log_today = TimelineLog.objects.create(
+            content_object=destruction_list,
+            template="logging/destruction_list_created.txt",
+            extra_data={},
+            user=record_manager,
+        )
+        log_today.timestamp = today
+        log_today.save()
+
+        log_two_days_ago = TimelineLog.objects.create(
+            content_object=destruction_list,
+            template="logging/destruction_list_created.txt",
+            extra_data={},
+            user=record_manager,
+        )
+        log_two_days_ago.timestamp = today - timedelta(days=2)
+        log_two_days_ago.save()
+
+        log_yesterday = TimelineLog.objects.create(
+            content_object=destruction_list,
+            template="logging/destruction_list_created.txt",
+            extra_data={},
+            user=record_manager,
+        )
+        log_yesterday.timestamp = today - timedelta(days=1)
+        log_yesterday.save()
+
+        endpoint = furl(reverse("api:logs-list"))
+        endpoint.args["destruction_list"] = destruction_list.uuid
+
+        self.client.force_login(record_manager)
+        response = self.client.get(endpoint.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(len(data), 3)
+        self.assertEqual(data[0]["pk"], log_two_days_ago.pk)
+        self.assertEqual(data[1]["pk"], log_yesterday.pk)
+        self.assertEqual(data[2]["pk"], log_today.pk)
