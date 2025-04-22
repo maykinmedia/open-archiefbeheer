41977ad6cbd7e412bd1e4d53a6c973d6f75b1825
SilviaAmAm <silvia@maykinmedia.nl>
2025-04-22 15:47:03 +0200

    :white_check_mark: [#827] Fix e2e tests

--- backend/src/openarchiefbeheer/destruction/tests/e2e/features/test_feature_configure_destruction_report.py
+++ backend/src/openarchiefbeheer/destruction/tests/e2e/features/test_feature_configure_destruction_report.py
@@ -27,7 +27,7 @@ class FeatureConfigureDestructionReport(GherkinLikeTestCase):
                 await self.when.user_clicks_button(page, "Vernietigingsrapport")
                 await self.then.path_should_be(page, "/settings/destruction-report")
 
-                await self.when.user_fills_form_field(page, "Bronorganisatie", "000000001")
+                await self.when.user_fills_form_field(page, "Bronorganisatie", "123456782")
                 await self.when.user_fills_form_field(page, "Zaaktype", "Aangifte behandelen 2")
                 await self.when.user_fills_form_field(page, "Statustype", "Statustype 2")
                 await self.when.user_fills_form_field(page, "Resultaattype", "Resultaattype 2")
@@ -37,7 +37,7 @@ class FeatureConfigureDestructionReport(GherkinLikeTestCase):
                 await self.then.page_should_contain_text(page, "De instellingen zijn succesvol opgeslagen")
                 await self.then.archive_configuration_should_be(
                     page,
-                    bronorganisatie="000000001",
+                    bronorganisatie="123456782",
                     zaaktype="ZAAKTYPE-02",
                     statustype="http://zaken.nl/catalogi/api/v1/statustypen/0b016f1a-e10a-4dad-9090-c06bac6ef7e7",
                     resultaattype="http://zaken.nl/catalogi/api/v1/resultaattypen/2af00ef7-d865-4166-9efc-19ab95fed618",
