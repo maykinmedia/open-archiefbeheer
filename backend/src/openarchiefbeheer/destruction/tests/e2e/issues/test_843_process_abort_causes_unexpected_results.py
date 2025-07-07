acae5f852eeeb52795596e8b5f050f1928f75e7d
Sven van de Scheur <svenvandescheur@gmail.com>
2025-07-07 12:18:12 +0200

    :white_check_mark: #843 - test: add additional test for changing record manager

--- backend/src/openarchiefbeheer/destruction/tests/e2e/issues/test_843_process_abort_causes_unexpected_results.py
+++ backend/src/openarchiefbeheer/destruction/tests/e2e/issues/test_843_process_abort_causes_unexpected_results.py
@@ -14,12 +14,12 @@ from ....models import DestructionList
 @tag("issue")
 @tag("gh-843")
 class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
-    async def _create(self, page: Page, reviewer_username="gh-843-reviewer-1"):
-        await self.given.record_manager_exists(username="gh-843-record-manager")
+    async def _create(self, page: Page, record_manager_username="gh-843-record-manager-1", reviewer_username="gh-843-reviewer-1"):
+        await self.given.record_manager_exists(username=record_manager_username)
         reviewer = await self.given.reviewer_exists(username=reviewer_username)
         await self.given.zaken_are_indexed(100)
 
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
+        await self.when.record_manager_logs_in(page, username="gh-843-record-manager-1")
         await self.when.user_clicks_button(page, "Vernietigingslijst opstellen")
         await self.when.user_clicks_checkbox(page, "(de)selecteer rij", index=0)
         await self.when.user_clicks_button(page, "Vernietigingslijst opstellen", index=1)
@@ -30,9 +30,9 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
         await self.then.path_should_be(page, "/destruction-lists")
         return await self.then.list_should_exist(page, "gh-843-destruction-list")
 
-    async def _ready_to_review(self, page: Page, destruction_list: DestructionList):
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+    async def _ready_to_review(self, page: Page, destruction_list: DestructionList, record_manager_username="gh-843-record-manager-1"):
+        await self.when.record_manager_logs_in(page, username=record_manager_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Ter beoordeling indienen")
         await self.when.user_clicks_button(page, "Ter beoordeling indienen", 1)
 
@@ -42,9 +42,9 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
 
         return destruction_list
 
-    async def _review_by_reviewer(self, page: Page, destruction_list: DestructionList, username="gh-843-reviewer-1"):
-        await self.when.reviewer_logs_in(page, username=username)
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+    async def _review_by_reviewer(self, page: Page, destruction_list: DestructionList, reviewer_username="gh-843-reviewer-1"):
+        await self.when.reviewer_logs_in(page, username=reviewer_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Goedkeuren")
         await self.when.user_fills_form_field(page, "Opmerking", "gh-843")
         await self.when.user_clicks_button(page, "Vernietigingslijst goedkeuren")
@@ -53,11 +53,11 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
         await destruction_list.arefresh_from_db()
         await self.then.list_should_have_status(page, destruction_list, ListStatus.internally_reviewed)
 
-    async def _finalize(self, page: Page, destruction_list: DestructionList, username="gh-843-archivist-1"):
-        archivaris = await self.given.archivist_exists(username=username)
+    async def _finalize(self, page: Page, destruction_list: DestructionList, record_manager_username="gh-843-record-manager-1", archivist_username="gh-843-archivist-1"):
+        archivaris = await self.given.archivist_exists(username=archivist_username)
 
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+        await self.when.record_manager_logs_in(page, username=record_manager_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Markeren als definitief")
         await self.when.user_fills_form_field(page, "Archivaris", str(archivaris))
         await self.when.user_fills_form_field(page, "Opmerking", "Dit is een test comment")
@@ -67,9 +67,9 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
         await destruction_list.arefresh_from_db()
         await self.then.list_should_have_status(page, destruction_list, ListStatus.ready_for_archivist)
 
-    async def _review_by_archivist(self, page: Page, destruction_list: DestructionList, username="gh-843-archivist-1"):
-        await self.when.archivist_logs_in(page, username=username)
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+    async def _review_by_archivist(self, page: Page, destruction_list: DestructionList, archivist_username="gh-843-archivist-1"):
+        await self.when.archivist_logs_in(page, username=archivist_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Goedkeuren")
         await self.when.user_fills_form_field(page, "Opmerking", "gh-843")
         await self.when.user_clicks_button(page, "Vernietigingslijst goedkeuren")
@@ -78,20 +78,22 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
         await destruction_list.arefresh_from_db()
         await self.then.list_should_have_status(page, destruction_list, ListStatus.ready_to_delete)
 
-    async def _schedule_destroy(self, page: Page, destruction_list: DestructionList):
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+    async def _schedule_destroy(self, page: Page, destruction_list: DestructionList, record_manager_username="gh-843-record-manager-1"):
+        await self.when.record_manager_logs_in(page, username=record_manager_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Vernietigen starten")
-        await self.when.user_fills_form_field(page, "Type naam van de lijst ter bevestiging", "gh-843-destruction-list")
+        await self.when.user_fills_form_field(page, "Type naam van de lijst ter bevestiging", destruction_list.name)
         await self.when.user_clicks_button(page, "1 zaken vernietigen")
 
         await self.then.path_should_be(page, "/destruction-lists")
         await destruction_list.arefresh_from_db()
         await self.then.list_should_have_status(page, destruction_list, ListStatus.ready_to_delete)
 
-    async def _abort_process(self, page: Page, destruction_list: DestructionList):
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+    async def _abort_process(self, page: Page, destruction_list: DestructionList, record_manager_username="gh-843-record-manager-1"):
+        await self.given.record_manager_exists(username=record_manager_username)
+
+        await self.when.record_manager_logs_in(page, username=record_manager_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Vernietigen annuleren")
         await self.when.user_fills_form_field(page, "Opmerking", "gh-843")
         await self.when.user_clicks_button(page, "Vernietiging annuleren")
@@ -105,11 +107,11 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
 
         await self.then.list_should_have_status(page, destruction_list, ListStatus.new)
 
-    async def _reassign(self, page: Page, destruction_list: DestructionList, username="gh-843-reviewer-2"):
-        reviewer2 = await self.given.reviewer_exists(username=username)
+    async def _reassign(self, page: Page, destruction_list: DestructionList, record_manager_username="gh-843-record-manager-1", reviewer_username="gh-843-reviewer-2"):
+        reviewer2 = await self.given.reviewer_exists(username=reviewer_username)
 
-        await self.when.record_manager_logs_in(page, username="gh-843-record-manager")
-        await self.when.user_clicks_button(page, "gh-843-destruction-list")
+        await self.when.record_manager_logs_in(page, username=record_manager_username)
+        await self.when.user_clicks_button(page, destruction_list.name)
         await self.when.user_clicks_button(page, "Beoordelaar bewerken")
         await self.when.user_fills_form_field(page, "Beoordelaar", str(reviewer2))
         await self.when.user_fills_form_field(page, "Reden", "gh-843")
@@ -133,7 +135,7 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
             # Second pass
             await self._reassign(page, destruction_list)
             await self._ready_to_review(page, destruction_list)
-            await self._review_by_reviewer(page, destruction_list, "gh-843-reviewer-2")
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="gh-843-reviewer-2")
             await self._finalize(page, destruction_list)
             await self._review_by_archivist(page, destruction_list)
             await self._schedule_destroy(page, destruction_list)
@@ -142,9 +144,9 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
             # Third pass
             await self._reassign(page, destruction_list)
             await self._ready_to_review(page, destruction_list)
-            await self._review_by_reviewer(page, destruction_list, "gh-843-reviewer-2")
-            await self._finalize(page, destruction_list, "gh-843-archivist-2")
-            await self._review_by_archivist(page, destruction_list, "gh-843-archivist-2")
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="gh-843-reviewer-2")
+            await self._finalize(page, destruction_list, archivist_username="gh-843-archivist-2")
+            await self._review_by_archivist(page, destruction_list, archivist_username="gh-843-archivist-2")
             await self._schedule_destroy(page, destruction_list)
             await self._abort_process(page, destruction_list)
 
@@ -153,20 +155,51 @@ class Issue843ProcessAbortCausesUnexpectedResultsTestCase(GherkinLikeTestCase):
         async with browser_page() as page:
             await self.given.user_exists(username="user a", post__can_review_destruction=True, post__can_review_final_list=True)
             await self.given.archivist_exists(username="user b")
-            await self.given.reviewer_exists(username="user c")
+            await self.given.user_exists(username="user c", post__can_review_destruction=True, post__can_review_final_list=True)
 
             # First pass
-            destruction_list = await self._create(page, "user a")
+            destruction_list = await self._create(page, reviewer_username="user a")
             await self._ready_to_review(page, destruction_list)
-            await self._review_by_reviewer(page, destruction_list, "user a")
-            await self._finalize(page, destruction_list, "user b")
-            await self._review_by_archivist(page, destruction_list, "user b")
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="user a")
+            await self._finalize(page, destruction_list, archivist_username="user b")
+            await self._review_by_archivist(page, destruction_list, archivist_username="user b")
             await self._schedule_destroy(page, destruction_list)
             await self._abort_process(page, destruction_list)
 
             # Second pass
-            await self._reassign(page, destruction_list, "user c")
+            await self._reassign(page, destruction_list, reviewer_username="user c")
             await self._ready_to_review(page, destruction_list)
-            await self._review_by_reviewer(page, destruction_list, "user c")
-            await self._finalize(page, destruction_list, "user a")
-            await self._review_by_archivist(page, destruction_list, "user a")
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="user c")
+            await self._finalize(page, destruction_list, archivist_username="user a")
+            await self._review_by_archivist(page, destruction_list, archivist_username="user a")
+
+    async def test_scenario_different_record_manager_on_second_pass(self):
+        async with browser_page() as page:
+            # First pass
+            destruction_list = await self._create(page)
+            await self._ready_to_review(page, destruction_list)
+            await self._review_by_reviewer(page, destruction_list)
+            await self._finalize(page, destruction_list)
+            await self._review_by_archivist(page, destruction_list)
+            await self._schedule_destroy(page, destruction_list)
+
+            await self._abort_process(page, destruction_list, "gh-843-record-manager-2")
+
+            # Second pass
+            await self._reassign(page, destruction_list, record_manager_username="gh-843-record-manager-2")
+            await self._ready_to_review(page, destruction_list, record_manager_username="gh-843-record-manager-2")
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="gh-843-reviewer-2")
+            await self._finalize(page, destruction_list, record_manager_username="gh-843-record-manager-2")
+            await self._review_by_archivist(page, destruction_list)
+            await self._schedule_destroy(page, destruction_list, record_manager_username="gh-843-record-manager-2")
+
+            await self._abort_process(page, destruction_list, record_manager_username="gh-843-record-manager-1")
+
+            # Third pass
+            await self._reassign(page, destruction_list)
+            await self._ready_to_review(page, destruction_list)
+            await self._review_by_reviewer(page, destruction_list, reviewer_username="gh-843-reviewer-2")
+            await self._finalize(page, destruction_list, archivist_username="gh-843-archivist-2")
+            await self._review_by_archivist(page, destruction_list, archivist_username="gh-843-archivist-2")
+            await self._schedule_destroy(page, destruction_list)
+            await self._abort_process(page, destruction_list)
