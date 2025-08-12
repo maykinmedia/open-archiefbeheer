dcfea02a56b982974efd2aa138ef55cc416baeca
SilviaAmAm <silvia@maykinmedia.nl>
2025-08-12 14:13:11 +0200

    :white_check_mark: [#841] Add test for pencil in archivist view

--- frontend/src/components/DestructionListArchivist/DestructionListArchivist.stories.tsx
+++ frontend/src/components/DestructionListArchivist/DestructionListArchivist.stories.tsx
@@ -1,10 +1,12 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { expect, userEvent, within } from "@storybook/test";
 
 import {
   ClearSessionStorageDecorator,
   ReactRouterDecorator,
 } from "../../../.storybook/decorators";
 import { MOCKS } from "../../../.storybook/mockData";
+import { fillForm } from "../../../.storybook/playFunctions";
 import {
   archivistFactory,
   destructionListFactory,
@@ -54,5 +56,71 @@ export const ReadyForArchivist: Story = {
         { user: ARCHIVIST, role: "archivist" },
       ],
     }),
+  },
+  play: async ({ context }) => {
+    await new Promise((resolve) => setTimeout(resolve, 600));
+    const editButton = await within(context.canvasElement).findByRole(
+      "button",
+      {
+        name: "Archivaris bewerken",
+      },
+    );
+    expect(editButton).toBeInTheDocument();
+
+    await userEvent.click(editButton);
+    const dialog = await within(context.canvasElement).findByRole("dialog");
+    const form = dialog.querySelector("form");
+    await fillForm({
+      ...context,
+      parameters: {
+        fillForm: {
+          form: form,
+          formValues: {
+            Archivaris: "Archi Varis (Archivaris)",
+            Reden: "Tada",
+          },
+          submitForm: true,
+        },
+      },
+    });
+  },
+};
+
+export const ReadyForArchivistSeenByArchivist: Story = {
+  args: {
+    destructionList: destructionListFactory({
+      author: RECORD_MANAGER,
+      assignee: ARCHIVIST,
+      status: "ready_for_archivist",
+      assignees: [
+        { user: RECORD_MANAGER, role: "author" },
+        { user: REVIEWER, role: "main_reviewer" },
+        { user: ARCHIVIST, role: "archivist" },
+      ],
+    }),
+  },
+  parameters: {
+    mockData: [
+      MOCKS.HEALTH_CHECK,
+      MOCKS.ARCHIVISTS,
+      MOCKS.OIDC_INFO,
+      MOCKS.INTERNAL_SELECTIE_LIJST_CHOICES,
+      {
+        url: "http://localhost:8000/api/v1/whoami/",
+        method: "GET",
+        status: 200,
+        response: ARCHIVIST,
+      },
+    ],
+  },
+  play: async ({ context }) => {
+    await new Promise((resolve) => setTimeout(resolve, 600));
+    const editButton = await within(context.canvasElement).queryByRole(
+      "button",
+      {
+        name: "Archivaris bewerken",
+      },
+    );
+    expect(editButton).not.toBeInTheDocument();
   },
 };
