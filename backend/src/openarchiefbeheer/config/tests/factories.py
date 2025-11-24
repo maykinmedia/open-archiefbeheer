1503ce1ee3a9ad89fbbd2d19d2f9f7041faed2b6
SilviaAmAm <silvia@maykinmedia.nl>
2025-11-24 15:47:06 +0100

    :white_check_mark: [#885] Update tests

--- backend/src/openarchiefbeheer/config/tests/factories.py
+++ backend/src/openarchiefbeheer/config/tests/factories.py
@@ -3,11 +3,11 @@ from factory.django import DjangoModelFactory
 from zgw_consumers.constants import APITypes
 from zgw_consumers.test.factories import ServiceFactory
 
-from ..models import APIConfig
+from ..models import APIConfig, ArchiveConfig
 
 
 # TODO: change to DjangoModelFactory[APIConfig]
-class APIConfigFactory(DjangoModelFactory):
+class SoloFactory(DjangoModelFactory):
     # Not mocking out the get_solo, as it would have hidden caching problems
     # Cache invalidation is hard, we shouldn't mock our way into a green CI.
     # note that this does come with test isolation issues.
@@ -19,7 +19,13 @@ class APIConfigFactory(DjangoModelFactory):
     # the rest, I don't expect to use any different values than the default
     # so it might not be a problem. Otherwise, running with pytest and have a
     # auto_use fixture that monkeypatches, is an option.
+    @classmethod
+    def _create(cls, model_class: type[APIConfig], *args, **kwargs):
+        model_class.get_solo().delete()
+        return super()._create(model_class, *args, **kwargs)
 
+
+class APIConfigFactory(SoloFactory):
     selectielijst_api_service = factory.SubFactory(  # pyright: ignore[reportPrivateImportUsage]
         ServiceFactory,
         api_root="https://selectielijst.openzaak.nl/api/v1",
@@ -29,7 +35,7 @@ class APIConfigFactory(DjangoModelFactory):
     class Meta:  # type: ignore
         model = APIConfig
 
-    @classmethod
-    def _create(cls, model_class: type[APIConfig], *args, **kwargs):
-        model_class.get_solo().delete()
-        return super()._create(model_class, *args, **kwargs)
+
+class ArchiveConfigFactory(SoloFactory):
+    class Meta:  # type: ignore
+        model = ArchiveConfig
