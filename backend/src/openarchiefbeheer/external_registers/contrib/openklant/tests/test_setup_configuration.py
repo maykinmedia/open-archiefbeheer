04c19de988bc9463226e6eb5f166ad80a858e03d
SilviaAmAm <silvia@maykinmedia.nl>
2025-11-27 16:58:10 +0100

    :white_check_mark: [#900] Test setup configuration for openklant

---
+++ backend/src/openarchiefbeheer/external_registers/contrib/openklant/tests/test_setup_configuration.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from django.test import TestCase
+
+from django_setup_configuration.test_utils import execute_single_step
+from pydantic import ValidationError
+from zgw_consumers.test.factories import ServiceFactory
+
+from openarchiefbeheer.external_registers.setup_configuration.steps import (
+    ExternalRegisterPluginsConfigurationStep,
+)
+from openarchiefbeheer.utils.tests.mixins import ClearCacheMixin
+
+from .factories import OpenKlantConfigFactory
+
+TEST_FILES = (Path(__file__).parent / "files").resolve()
+CONFIG_FILE_PATH = str(TEST_FILES / "external_registers.yaml")
+BROKEN_CONFIG_FILE_PATH = str(TEST_FILES / "broken_config.yaml")
+
+
+class ExternalRegistersConfigurationStepTests(ClearCacheMixin, TestCase):
+    def test_configure_external_registers(self):
+        ServiceFactory.create(slug="openklant_service1")
+        ServiceFactory.create(slug="openklant_service2")
+        config = OpenKlantConfigFactory.create()
+
+        execute_single_step(
+            ExternalRegisterPluginsConfigurationStep, yaml_source=CONFIG_FILE_PATH
+        )
+
+        config.refresh_from_db()
+
+        self.assertTrue(config.enabled)
+        self.assertEqual(config.services.count(), 2)
+        self.assertEqual(
+            list(config.services.all().order_by("slug").values_list("slug", flat=True)),
+            ["openklant_service1", "openklant_service2"],
+        )
+
+    def test_wrong_configuration(self):
+        with self.assertRaises(
+            ValidationError,
+            msg="1 validation error for OpenKlantConfigurationModel services_identifiers",
+        ):
+            execute_single_step(
+                ExternalRegisterPluginsConfigurationStep,
+                yaml_source=BROKEN_CONFIG_FILE_PATH,
+            )
