5df8f985647e84253246b604770297f918fc264b
SilviaAmAm <silvia@maykinmedia.nl>
2025-12-09 17:19:28 +0100

    :white_check_mark: [#901] Trying to test getting the plugins

--- backend/src/openarchiefbeheer/destruction/tests/test_views.py
+++ backend/src/openarchiefbeheer/destruction/tests/test_views.py
@@ -1,8 +1,15 @@
+from djangorestframework_camel_case.parser import CamelCaseJSONParser
+from djangorestframework_camel_case.util import underscoreize
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+from zgw_consumers.constants import APITypes, AuthTypes
+from zgw_consumers.test.factories import ServiceFactory
 
 from openarchiefbeheer.accounts.tests.factories import UserFactory
+from openarchiefbeheer.destruction.tests.factories import DestructionListItemFactory
+from openarchiefbeheer.utils.tests.resources_client import OpenZaakDataCreationHelper
+from openarchiefbeheer.zaken.api.serializers import ZaakSerializer
 
 
 class StatusViewTests(APITestCase):
@@ -22,3 +29,156 @@ class StatusViewTests(APITestCase):
         response = self.client.get(endpoint)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RelatedObjectsViewTests(APITestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+        cls.zrc_service = ServiceFactory.create(
+            slug="zaken",
+            api_type=APITypes.zrc,
+            api_root="http://localhost:8003/zaken/api/v1",
+            client_id="test-vcr",
+            secret="test-vcr",
+        )
+        cls.ztc_service = ServiceFactory.create(
+            slug="catalogi",
+            api_type=APITypes.ztc,
+            api_root="http://localhost:8003/catalogi/api/v1",
+            client_id="test-vcr",
+            secret="test-vcr",
+        )
+        cls.openklant_service = ServiceFactory.create(
+            slug="openklant",
+            api_type=APITypes.orc,
+            api_root="http://localhost:8005/klantinteracties/api/v1/",
+            auth_type=AuthTypes.api_key,
+            header_key="Authorization",
+            header_value="Token ba9d233e95e04c4a8a661a27daffe7c9bd019067",
+        )
+
+    def test_not_authenticated(self):
+        endpoint = reverse("api:destruction-items-relations", args=(1,))
+
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_not_supported_relation(self):
+        user = UserFactory.create()
+
+        helper = OpenZaakDataCreationHelper(
+            zrc_service_slug="zaken",
+            ztc_service_slug="catalogi",
+            drc_service_slug="documents",
+            openklant_service_slug="openklant",
+        )
+        resources = helper.create_zaaktype_with_relations()
+        zaaktype = resources["zaaktype"]
+        assert isinstance(zaaktype["url"], str) and isinstance(
+            zaaktype["catalogus"], str
+        )
+
+        helper.publish_zaaktype(zaaktype["url"])
+
+        zaak = helper.create_zaak(zaaktype_url=zaaktype["url"])
+
+        assert isinstance(zaak, dict) and isinstance(zaak["url"], str)
+        zaakobject = helper.create_zaakobject(
+            zaak_url=zaak["url"],
+            object_url="https://api.chucknorris.io/jokes/3kcaD8EnSKuRHKAi9Lt8HQ",
+            **{
+                "objectTypeOverige": "Chuck norris quote",
+                "objectIdentificatie": {"overigeData": "3kcaD8EnSKuRHKAi9Lt8HQ"},
+            },
+        )
+
+        serializer = ZaakSerializer(
+            data=underscoreize(zaak, **CamelCaseJSONParser.json_underscoreize)
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        item = DestructionListItemFactory.create(zaak=serializer.instance)
+
+        endpoint = reverse("api:destruction-items-relations", args=(item.pk,))
+        self.client.force_authenticate(user)
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0],
+            {
+                "url": zaakobject["url"],
+                "selected": True,
+                "supported": False,
+                "result": zaakobject,
+            },
+        )
+
+    def test_supported_relation(self):
+        # TODO figure out a way on how to test this with the Docker containers.
+        user = UserFactory.create()
+
+        helper = OpenZaakDataCreationHelper(
+            zrc_service_slug="zaken",
+            ztc_service_slug="catalogi",
+            drc_service_slug="documents",
+            openklant_service_slug="openklant",
+        )
+        resources = helper.create_zaaktype_with_relations()
+        zaaktype = resources["zaaktype"]
+        assert isinstance(zaaktype["url"], str) and isinstance(
+            zaaktype["catalogus"], str
+        )
+
+        helper.publish_zaaktype(zaaktype["url"])
+
+        zaak = helper.create_zaak(zaaktype_url=zaaktype["url"])
+        klantcontact = helper.create_klantcontact()
+        assert (
+            isinstance(zaak, dict)
+            and isinstance(zaak["url"], str)
+            and isinstance(klantcontact["url"], str)
+        )
+        zaakobject = helper.create_zaakobject(
+            zaak_url=zaak["url"],
+            object_url=klantcontact["url"],
+            **{
+                "objectTypeOverige": "Klantcontact",
+                "objectIdentificatie": {"overigeData": "Boh"},
+            },
+        )
+
+        serializer = ZaakSerializer(
+            data=underscoreize(zaak, **CamelCaseJSONParser.json_underscoreize)
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        item = DestructionListItemFactory.create(zaak=serializer.instance)
+
+        endpoint = reverse("api:destruction-items-relations", args=(item.pk,))
+        self.client.force_authenticate(user)
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0],
+            {
+                "url": zaakobject["url"],
+                "selected": True,
+                "supported": False,
+                "result": zaakobject,
+            },
+        )
---
+++ backend/src/openarchiefbeheer/utils/tests/resources_client.py
@@ -0,0 +1,344 @@
+import string
+from dataclasses import dataclass
+from random import choice
+from typing import ContextManager, Iterable, Mapping, Sequence, TypedDict
+
+from ape_pie import APIClient
+from faker import Faker
+from faker.providers import lorem
+from furl import furl
+from zgw_consumers.client import build_client
+from zgw_consumers.models import Service
+
+from openarchiefbeheer.clients import drc_client, zrc_client, ztc_client
+
+faker = Faker()
+faker.add_provider(lorem)
+
+type JSONEncodable = (
+    None
+    | bool
+    | int
+    | float
+    | str
+    | Sequence[JSONEncodable]
+    | Mapping[str, JSONEncodable]
+)
+
+
+class ZaaktypeWithRelations(TypedDict):
+    zaaktype: Mapping[str, JSONEncodable]
+    roltype: Mapping[str, JSONEncodable]
+    resultaattype: Mapping[str, JSONEncodable]
+    statustypen: Iterable[Mapping[str, JSONEncodable]]
+
+
+@dataclass
+class OpenZaakDataCreationHelper:
+    zrc_service_slug: str
+    ztc_service_slug: str
+    drc_service_slug: str
+    openklant_service_slug: str
+
+    def _create_resource(
+        self,
+        data: Mapping[str, JSONEncodable],
+        resource_path: str,
+        client_cm: ContextManager[APIClient],
+    ) -> Mapping[str, JSONEncodable]:
+        with client_cm as client:
+            response = client.post(
+                resource_path,
+                json=data,
+                headers={
+                    "Accept-Crs": "EPSG:4326",
+                    "Content-Crs": "EPSG:4326",
+                },
+            )
+
+            if response.status_code == 400:
+                raise Exception(response.content)
+
+            response.raise_for_status()
+
+        return response.json()
+
+    def create_zaak(
+        self, zaaktype_url: str = "", **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        if not zaaktype_url:
+            zaaktype = self.create_zaaktype()
+            assert isinstance(zaaktype, dict) and isinstance(zaaktype["url"], str)
+            zaaktype_url = zaaktype["url"]
+
+        data: Mapping[str, JSONEncodable] = {
+            "zaaktype": zaaktype_url,
+            "bronorganisatie": "000000000",
+            "verantwoordelijkeOrganisatie": "000000000",
+            "startdatum": "2000-01-01",
+            "archiefnominatie": "vernietigen",
+            "archiefactiedatum": "2020-01-01",
+        }
+
+        return self._create_resource(
+            data | overrides, "zaken", zrc_client(self.zrc_service_slug)
+        )
+
+    def _get_catalogus(self, catalogus_url="", **_) -> str:
+        if catalogus_url:
+            return catalogus_url
+
+        catalogus = self.create_catalogus()
+        assert isinstance(catalogus, dict)
+        catalogus_url = catalogus["url"]
+        assert isinstance(catalogus_url, str)
+        return catalogus_url
+
+    def create_catalogus(
+        self, **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "domein": "".join([choice(string.ascii_uppercase) for _ in range(5)]),
+            "rsin": "123456782",
+            "contactpersoonBeheerNaam": "Ubaldo",
+            "naam": faker.sentence(),
+        } | overrides
+
+        return self._create_resource(
+            data, "catalogussen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_zaaktype_with_relations(
+        self,
+        catalogus_url: str = "",
+        objecttype_url: str = "",
+        **zaaktype_overrides: Mapping[str, JSONEncodable],
+    ) -> ZaaktypeWithRelations:
+        zaaktype = self.create_zaaktype(catalogus_url, **zaaktype_overrides)
+        assert isinstance(zaaktype["url"], str)
+
+        roltype = self.create_roltype(zaaktype_url=zaaktype["url"])
+        resultaattype = self.create_resultaattype(zaaktype_url=zaaktype["url"])
+        statustype1 = self.create_statustype(
+            zaaktype_url=zaaktype["url"],
+            **{"omschrijving": "begin", "volgnummer": 1},
+        )
+        statustype2 = self.create_statustype(
+            zaaktype_url=zaaktype["url"],
+            **{"omschrijving": "eind", "volgnummer": 2},
+        )
+
+        return {
+            "zaaktype": zaaktype,
+            "resultaattype": resultaattype,
+            "statustypen": [statustype1, statustype2],
+            "roltype": roltype,
+        }
+
+    def publish_zaaktype(self, zaaktype_url: str) -> None:
+        with ztc_client(self.ztc_service_slug) as client:
+            uuid = furl(zaaktype_url).path.segments[-1]
+            response = client.post(f"zaaktypen/{uuid}/publish")
+
+            if response.status_code == 400:
+                raise Exception(response.json())
+
+            response.raise_for_status()
+
+    def publish_informatieobjecttype(self, informatieobjecttype_url: str) -> None:
+        with ztc_client(self.ztc_service_slug) as client:
+            uuid = furl(informatieobjecttype_url).path.segments[-1]
+            response = client.post(f"informatieobjecttypen/{uuid}/publish")
+
+            if response.status_code == 400:
+                raise Exception(response.json())
+
+            response.raise_for_status()
+
+    def create_zaaktype(
+        self, catalogus: str = "", **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "omschrijving": faker.sentence(),
+            "vertrouwelijkheidaanduiding": "geheim",
+            "doel": "New Zaaktype 001",
+            "aanleiding": "New Zaaktype 001",
+            "indicatieInternOfExtern": "intern",
+            "handelingInitiator": "aanvragen",
+            "onderwerp": "New Zaaktype 001",
+            "handelingBehandelaar": "handelin",
+            "doorlooptijd": "P40D",
+            "opschortingEnAanhoudingMogelijk": False,
+            "verlengingMogelijk": True,
+            "verlengingstermijn": "P40D",
+            "publicatieIndicatie": False,
+            "productenOfDiensten": ["https://example.com/product/321"],
+            "referentieproces": {"naam": "ReferentieProces 1"},
+            "verantwoordelijke": "200000000",
+            "beginGeldigheid": "2025-06-19",
+            "versiedatum": "2025-06-19",
+            "catalogus": self._get_catalogus(catalogus),
+            "besluittypen": [],
+            "gerelateerdeZaaktypen": [],
+            "selectielijstProcestype": "https://selectielijst.openzaak.nl/api/v1/procestypen/aa8aa2fd-b9c6-4e34-9a6c-58a677f60ea0",
+        } | overrides
+
+        return self._create_resource(
+            data, "zaaktypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_roltype(
+        self,
+        zaaktype_url: str = "",
+        **overrides: Mapping[str, JSONEncodable],
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "zaaktype": zaaktype_url,
+            "omschrijving": "Vastgesteld",
+            "omschrijvingGeneriek": "adviseur",
+            **overrides,
+        }
+        return self._create_resource(
+            data, "roltypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_statustype(
+        self, zaaktype_url: str = "", **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "omschrijving": "Ontvangen",
+            "zaaktype": zaaktype_url,
+            "volgnummer": 1,
+            "omschrijving_generiek": "",
+            "statustekst": "",
+            "informeren": False,
+            "checklistitem_statustype": [],
+        } | overrides
+
+        return self._create_resource(
+            data, "statustypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_resultaattype(
+        self,
+        zaaktype_url: str = "",
+        **overrides: Mapping[str, JSONEncodable],
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "zaaktype": zaaktype_url,
+            "omschrijving": "Gegrond",
+            "resultaattypeomschrijving": "https://selectielijst.openzaak.nl/api/v1/resultaattypeomschrijvingen/3a0a9c3c-0847-4e7e-b7d9-765b9434094c",
+            "selectielijstklasse": "https://selectielijst.openzaak.nl/api/v1/resultaten/8af64c99-a168-40dd-8afd-9fbe0597b6dc",
+            "archiefnominatie": "vernietigen",
+            "brondatumArchiefprocedure": {
+                "afleidingswijze": "afgehandeld",
+                "procestermijn": None,
+                "datumkenmerk": "",
+                "einddatumBekend": False,
+                "objecttype": "",
+                "registratie": "",
+            },
+        } | overrides
+
+        return self._create_resource(
+            data, "resultaattypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_informatieobjecttype(
+        self, catalogus_url: str = "", **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "catalogus": catalogus_url,
+            "omschrijving": "Omschrijving A",
+            "vertrouwelijkheidaanduiding": "openbaar",
+            "beginGeldigheid": "2025-07-01",
+            "informatieobjectcategorie": "Blue",
+        } | overrides
+
+        return self._create_resource(
+            data, "informatieobjecttypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_zaakobjecttype(
+        self,
+        zaaktype_url: str,
+        objecttype_url: str,
+        **overrides: Mapping[str, JSONEncodable],
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "anderObjecttype": False,
+            "objecttype": objecttype_url,
+            "relatieOmschrijving": "Some relation",
+            "zaaktype": zaaktype_url,
+        } | overrides
+
+        return self._create_resource(
+            data, "zaakobjecttypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def relate_zaaktype_informatieobjecttype(
+        self,
+        zaaktype_url: str,
+        informatieobjecttype_url: str,
+        **overrides: Mapping[str, JSONEncodable],
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "zaaktype": zaaktype_url,
+            "informatieobjecttype": informatieobjecttype_url,
+            "volgnummer": 1,
+            "richting": "inkomend",
+        } | overrides
+
+        return self._create_resource(
+            data, "zaaktype-informatieobjecttypen", ztc_client(self.ztc_service_slug)
+        )
+
+    def create_zaakobject(
+        self,
+        zaak_url: str,
+        object_url: str,
+        **overrides: Mapping[str, JSONEncodable],
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "zaak": zaak_url,
+            "objectType": "overige",
+            "object": object_url,
+            "objectTypeOverige": "A description of the type",
+            "objectIdentificatie": {"overigeData": "Bla"},
+        } | overrides
+
+        return self._create_resource(
+            data, "zaakobjecten", zrc_client(self.zrc_service_slug)
+        )
+
+    def create_enkelvoudiginformatieobject(
+        self, informatieobjecttype_url: str, **overrides: Mapping[str, JSONEncodable]
+    ):
+        data: Mapping[str, JSONEncodable] = {
+            "bronorganisatie": "000000000",
+            "creatiedatum": "2020-01-01",
+            "titel": "Report on testing",
+            "auteur": "OAB test tool",
+            "taal": "dut",
+            "informatieobjecttype": informatieobjecttype_url,
+        } | overrides
+
+        return self._create_resource(
+            data, "enkelvoudiginformatieobjecten", drc_client(self.drc_service_slug)
+        )
+
+    def create_klantcontact(
+        self, **overrides: Mapping[str, JSONEncodable]
+    ) -> Mapping[str, JSONEncodable]:
+        data: Mapping[str, JSONEncodable] = {
+            "kanaal": "telefoon",
+            "onderwerp": "Testing",
+            "taal": "dut",
+            "vertrouwelijk": False,
+        } | overrides
+
+        return self._create_resource(
+            data,
+            "klantcontacten",
+            build_client(Service.objects.get(slug=self.openklant_service_slug)),
+        )
