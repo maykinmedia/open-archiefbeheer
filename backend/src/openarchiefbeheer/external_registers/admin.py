4e5555d6689e868f46289c4336aee5ec5f6017c6
SilviaAmAm <silvia@maykinmedia.nl>
2025-12-15 11:35:48 +0100

    :safety_vest: [#901] Add validation that services should not be shared across plugins

--- backend/src/openarchiefbeheer/external_registers/admin.py
+++ backend/src/openarchiefbeheer/external_registers/admin.py
@@ -1,7 +1,38 @@
+from typing import NoReturn
+
+from django import forms
 from django.contrib import admin
+from django.db.models import Q
 from django.http import HttpRequest
+from django.utils.translation import gettext as _
+
+from zgw_consumers.models import Service
 
 from .models import ExternalRegisterConfig
+
+
+class ExternalRegisterConfigAdminForm(forms.ModelForm):
+    class Meta:
+        fields = ("identifier", "enabled", "services")
+
+    def clean(self) -> dict[str, list[Service] | bool | str] | NoReturn:
+        cleaned_data = super().clean()
+
+        if updated_services := cleaned_data.get("services", []):
+            service_pks = [service.pk for service in updated_services]
+            qs = ExternalRegisterConfig.objects.filter(services__id__in=service_pks)
+            if self.instance:
+                qs = qs.filter(~Q(pk=self.instance.pk))
+            if qs.count() > 0:
+                # TODO: Improve so that it says which service and which plugin.
+                raise forms.ValidationError(
+                    _(
+                        "One or more of the selected services are already used by another plugin"
+                    ),
+                    code="invalid",
+                )
+
+        return cleaned_data
 
 
 @admin.register(ExternalRegisterConfig)
@@ -12,6 +43,7 @@ class ExternalRegisterConfigAdmin(admin.ModelAdmin):
     )
     search_fields = ("identifier",)
     readonly_fields = ("identifier",)
+    form = ExternalRegisterConfigAdminForm
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return False
