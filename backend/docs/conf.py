ca8e1c952e5db4a2849888d6e9a5bed169584b25
SilviaAmAm <silvia@maykinmedia.nl>
2026-01-09 12:43:19 +0100

    :wrench: [#957] Use pyproject.toml to populate pythonpath

--- backend/docs/conf.py
+++ backend/docs/conf.py
@@ -11,7 +11,7 @@ import sys
 
 import django
 
-root_folder = pathlib.Path(__file__).parent
+root_folder = pathlib.Path(__file__).parent.parent
 src_folder = root_folder / pathlib.Path("src")
 
 sys.path.insert(0, str(src_folder))
