792f2b94487a28c0a54f332166759cd6e43a31cf
SilviaAmAm <silvia@maykinmedia.nl>
2026-01-22 16:53:23 +0100

    :white_check_mark: [#978] Test deleting from Objecten API
    After the addition of the zaak query parameter in the DELETE endpoint

--- backend/src/openarchiefbeheer/external_registers/contrib/objecten/tests/test_plugin.py
+++ backend/src/openarchiefbeheer/external_registers/contrib/objecten/tests/test_plugin.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+import requests_mock
 from vcr.unittest import VCRMixin
 from zgw_consumers.constants import APITypes, AuthTypes
 from zgw_consumers.test.factories import ServiceFactory
@@ -73,10 +74,18 @@ class ObjectenPluginTests(ClearCacheMixin, VCRMixin, TestCase):
         config.save()
 
         plugin = register[OBJECTEN_IDENTIFIER]
-        plugin.delete_related_resources(
-            item=item,
-            related_resources=[object_resource["url"]],
-        )
+        # TODO: test with real Objecten API once https://github.com/maykinmedia/objects-api/pull/719 is
+        # merged and released!
+        with requests_mock.Mocker(real_http=True) as m:
+            m.delete(
+                f"{object_resource['url']}?"
+                "zaak=http://localhost:8003/zaken/api/v1/zaken/111-111-111",
+                status_code=204,
+            )
+            plugin.delete_related_resources(
+                item=item,
+                related_resources=[object_resource["url"]],
+            )
 
         result = ResourceDestructionResult.objects.get(
             item=item,
@@ -88,3 +97,45 @@ class ObjectenPluginTests(ClearCacheMixin, VCRMixin, TestCase):
             result.url,
             object_resource["url"],
         )
+
+    def test_unlink_relations(self):
+        service = ServiceFactory.create(
+            slug="objecten",
+            api_type=APITypes.orc,
+            api_root="http://localhost:8006/api/v2/",
+            auth_type=AuthTypes.api_key,
+            header_key="Authorization",
+            header_value="Token ba9d233e95e04c4a8a661a27daffe7c9bd019067",
+        )
+        item = DestructionListItemFactory.create(
+            with_zaak=True,
+            zaak__url="http://localhost:8003/zaken/api/v1/zaken/111-111-111",
+        )
+        config = ExternalRegisterConfig.objects.get(identifier=OBJECTEN_IDENTIFIER)
+        config.enabled = True
+        config.services.add(service)
+        config.save()
+
+        plugin = register[OBJECTEN_IDENTIFIER]
+
+        # TODO: test with real Objecten API once https://github.com/maykinmedia/objects-api/pull/719 is
+        # merged and released!
+        with requests_mock.Mocker() as m:
+            m.delete(
+                "http://localhost:8006/api/v2/objects/5e8e9d9f-dbab-475f-8a8b-3e5a1b74d3ab?"
+                "zaak=http://localhost:8003/zaken/api/v1/zaken/111-111-111",
+                status_code=200,
+            )
+            plugin.delete_related_resources(
+                item=item,
+                related_resources=[
+                    "http://localhost:8006/api/v2/objects/5e8e9d9f-dbab-475f-8a8b-3e5a1b74d3ab"
+                ],
+            )
+
+        result = ResourceDestructionResult.objects.get(
+            item=item,
+            resource_type="objecten",
+        )
+
+        self.assertEqual(result.status, ResourceDestructionResultStatus.unlinked)
