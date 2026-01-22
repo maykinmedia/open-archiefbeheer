c0807ac8de9c0dbd9fa9b0ee250ce68cce669e99
Sven van de Scheur <svenvandescheur@gmail.com>
2026-01-22 12:03:24 +0100

    :white_check_mark: test: fix the tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,17 @@ import { render } from "@testing-library/react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { HealthCheckResult } from "./lib/api/health-check";
+import { mockResponseOnce } from "./lib/test/mockResponse";
 
 test("renders app", () => {
+  const result: HealthCheckResult = {
+    message: "",
+    success: true,
+    identifier: "",
+  };
+  mockResponseOnce("get", "http://localhost:8000/api/v1/health-check", result);
+
   const router = createBrowserRouter([
     {
       path: "*",
