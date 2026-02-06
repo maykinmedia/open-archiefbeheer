a989cbc5e2052fc2e287baf034694d0ae50bd8bf
SilviaAmAm <silvia@maykinmedia.nl>
2026-02-06 12:14:05 +0100

    :sparkles: Add default OTEL_SERVICE_NAME for the various components
    This otherwise gives warnings in the logs.

--- backend/bin/celery_beat.sh
+++ backend/bin/celery_beat.sh
@@ -4,6 +4,9 @@ set -e
 
 LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 
+# Set defaults for OTEL
+export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-openarchiefbeheer-beat}"
+
 mkdir -p celerybeat
 
 echo "Starting celery beat"
