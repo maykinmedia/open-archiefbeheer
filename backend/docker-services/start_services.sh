b369e44be6c5e7a7ac84d81c418fd35cd2467b4e
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-09-08 15:04:45 +0200

    :wrench: [#1088] Update linux docker check
    The previous check doesn't work for docker desktop, so you can never start the services.
    The start commands still won't work, but I can start the desktop version manually. I asked ChatGPT for
    a script which checks which variant is installed, but it seems to be a bit annoying and not worth the
    effort.

--- backend/docker-services/start_services.sh
+++ backend/docker-services/start_services.sh
@@ -25,7 +25,7 @@ check_docker() {
 }
 
 check_docker_linux() {
-    if ! curl -s -X GET http://127.0.0.1:2375/_ping | grep -q "OK"; then
+   if ! docker info >/dev/null 2>&1; then
         log_info "Docker not running. Restarting..."
         sudo systemctl stop docker.service
         sudo dockerd \
