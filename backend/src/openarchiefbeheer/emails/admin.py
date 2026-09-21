c94717321a7222adb40a7622977be0dc666ccf83
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-09-21 10:26:46 +0200

    :sparkles: [#1086] Add validation for e-mail templates
    This will avoid 500 errors when trying to send e-mails with invalid templates.

--- backend/src/openarchiefbeheer/emails/admin.py
+++ backend/src/openarchiefbeheer/emails/admin.py
@@ -1,13 +1,58 @@
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.template import TemplateSyntaxError
 from django.utils.translation import gettext_lazy as _
 
 from solo.admin import SingletonModelAdmin
 
 from .models import EmailConfig
+from .render_backend import get_sandboxed_backend
+
+
+class EmailConfigForm(forms.ModelForm):
+    TEMPLATE_FIELDS_TO_VALIDATE = [
+        "body_review_required_html",
+        "body_review_required_text",
+        "body_co_review_request_html",
+        "body_co_review_request_text",
+        "body_review_reminder_html",
+        "body_review_reminder_text",
+        "body_changes_requested_html",
+        "body_changes_requested_text",
+        "body_positive_review_html",
+        "body_positive_review_text",
+        "body_error_during_deletion_html",
+        "body_error_during_deletion_text",
+        "body_successful_deletion_html",
+        "body_successful_deletion_text",
+    ]
+
+    class Meta:
+        model = EmailConfig
+        fields = "__all__"
+
+    def clean(self):
+        cleaned_data = super().clean()
+        assert cleaned_data is not None
+
+        backend = get_sandboxed_backend()
+        errors = {}
+        for field in self.TEMPLATE_FIELDS_TO_VALIDATE:
+            try:
+                backend.from_string(cleaned_data[field])
+            except TemplateSyntaxError as e:  # noqa: PERF203
+                errors[field] = _("Error in template: {e}").format(e=e)
+
+        if errors:
+            raise ValidationError(errors, code="invalid-template")
+
+        return cleaned_data
 
 
 @admin.register(EmailConfig)
 class EmailConfigAdmin(SingletonModelAdmin):
+    form = EmailConfigForm
     fieldsets = [
         (
             _("Template review request"),
